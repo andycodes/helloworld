89920ec3e7365c8733bf20d39ebe402ad2780e5e
andy <brucefan@foxmail.com>
2019-05-04 10:45:44 +0800

    netlink kernel

--- bsp/os/linuxkernel/drivers/opc/netlink/netlink.c
+++ bsp/os/linuxkernel/drivers/opc/netlink/netlink.c
@@ -4,9 +4,13 @@
 #include <linux/sched.h>
 #include <net/sock.h>
 #include <net/netlink.h>
+
 #define NETLINK_TEST 28
 struct sock *nl_sk = NULL;
 EXPORT_SYMBOL_GPL(nl_sk);
+
+const char* kernel_send_str = "data form kernel wowo";
+
 void nl_data_ready (struct sk_buff *__skb)
 {
 	struct sk_buff *skb;
@@ -15,10 +19,12 @@ void nl_data_ready (struct sk_buff *__skb)
 	int rc;
 	int len = NLMSG_SPACE(1200);
 	char str[100];
-	printk("net_link: data is ready to read.\n");
+
+	printk("netlink_kernel_create callback.\n");
 	skb = skb_get(__skb);
 	if (skb->len >= NLMSG_SPACE(0)) {
-		nlh = nlmsg_hdr(skb);
+		nlh = nlmsg_hdr(skb);/*获取netlink数据包中netlink header的起始地址*/
+		/*宏定义NLMSG_DATA(nlh)返回指向netlink消息体的payload的指针*/
 		memcpy(str,NLMSG_DATA(nlh), sizeof(str));
 		printk("[%s]recv data:[%s]\n", __func__, str);
 		pid = nlh->nlmsg_pid; /*pid of sending process */
@@ -30,15 +36,18 @@ void nl_data_ready (struct sk_buff *__skb)
 			printk(KERN_ERR "net_link: allocate failed.\n");
 			return;
 		}
+
 		nlh = nlmsg_put(skb,0,0,0,1200,0);
 		NETLINK_CB(skb).portid = 0; /* from kernel */
-		memcpy(NLMSG_DATA(nlh), "data form kernel wowo", sizeof("data form kernel wowo"));
+		memcpy(NLMSG_DATA(nlh), kernel_send_str, strlen(kernel_send_str));
 		rc = netlink_unicast(nl_sk, skb, pid, MSG_DONTWAIT);
-		if (rc < 0)
+		if (rc < 0) {
 			printk(KERN_ERR "net_link: can not unicast skb (%d)\n", rc);
+		}
 	}
 }
-static int test_netlink(void) 
+
+int init_module()
 {
 	struct netlink_kernel_cfg cfg = {
 		.input		= nl_data_ready,
@@ -46,23 +55,21 @@ static int test_netlink(void)
 
 	nl_sk = netlink_kernel_create(&init_net, NETLINK_TEST, &cfg);
 	if (!nl_sk) {
-	printk(KERN_ERR "net_link: Cannot create netlink socket.\n");
-	return -EIO;
+		printk(KERN_ERR "net_link: Cannot create netlink socket.\n");
+		return -EIO;
 	}
-	printk("net_link: create socket ok.\n");
+
+	printk(" create socket ok.\n");
 	return 0;
 }
-int init_module()
-{
-  test_netlink();
-  return 0;
-}
+
 void cleanup_module( )
 {
-  if (nl_sk != NULL){
-    sock_release(nl_sk->sk_socket);
-  }
-  printk("net_link: remove ok.\n");
+	if (nl_sk != NULL){
+		sock_release(nl_sk->sk_socket);
+	}
+
+	printk("net_link: remove ok.\n");
 }
 
 MODULE_LICENSE("Dual BSD/GPL");
