8e1e0a523b5cfc26f1ede9b320e40af6f369a116
andycodes <brucefan@foxmail.com>
2019-05-17 21:05:55 +0800

    del ai

--- ai/tensorflow/truncated_normal.py
+++
@@ -1,9 +0,0 @@
-
-import tensorflow as tf;
-import numpy as np;
-import matplotlib.pyplot as plt;
- 
-c = tf.truncated_normal(shape=[10,10], mean=0, stddev=1)
- 
-with tf.Session() as sess:
-	print sess.run(c)
