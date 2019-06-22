ffff65d3b0b0758a663e206e0b720e078c5ab12d
andycodes <brucefan@foxmail.com>
2019-06-22 21:33:49 +0800

    makefile

--- mcu/main.c
+++ mcu/main.c
@@ -1,7 +1,7 @@
 /*************************************************************************
     > File Name: main.c
     > Author: shift
-    > Mail: open_shift@163.com 
+    > Mail: open_shift@163.com
     > Created Time: 2015年03月03日 星期二 20时30分38秒
  ************************************************************************/
 
@@ -18,7 +18,7 @@ int main()
 	printf("int a+b IS:%d\n", add_int(a, b));
 	printf("int a-b IS:%d\n", sub_int(a, b));
 	printf("float x+y IS:%f\n", add_float(x, y));
-	printf("float x-y IS:%f\n", sub_float(x, y));
+//	printf("float x-y IS:%f\n", sub_float(x, y));
 
 	return 0;
 }
