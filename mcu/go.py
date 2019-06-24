f4591eb95cc8c6208d8145beb7aa94f70f18767a
andycodes <brucefan@foxmail.com>
2019-06-24 21:57:22 +0800

    go

--- mcu/go.py
+++ mcu/go.py
@@ -3,9 +3,11 @@
 import os
 import shutil
 OUT_PATH="./out"
-OBJ_PATH= OUT_PATH +"/obj"
+OUT_OBJ_PATH= OUT_PATH +"/obj"
+OUT_LIBS_PATH = OUT_PATH +"/libs"
 if os.path.exists(OUT_PATH):
 	shutil.rmtree(OUT_PATH)
 
-os.makedirs(OBJ_PATH)
+os.makedirs(OUT_OBJ_PATH)
+os.makedirs(OUT_LIBS_PATH)
 os.system('make')
