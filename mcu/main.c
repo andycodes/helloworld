7d2453930935bac208b6aee3031b2d7c0538d051
andycodes <brucefan@foxmail.com>
2019-06-24 22:01:31 +0800

    so

--- mcu/main.c
+++ mcu/main.c
@@ -18,7 +18,6 @@ int main()
 	printf("int a+b IS:%d\n", add_int(a, b));
 	printf("int a-b IS:%d\n", sub_int(a, b));
 	printf("float x+y IS:%f\n", add_float(x, y));
-//	printf("float x-y IS:%f\n", sub_float(x, y));
 
 	return 0;
 }
