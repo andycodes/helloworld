89c3852b91d1d8edf50afb75d6ed79e74f736cc9
andycodes <brucefan@foxmail.com>
2019-07-05 21:45:04 +0800

    kernel fs

--- bsp/linuxkernel/kernel/fs/fileoperate/fileoperate.c
+++ bsp/linuxkernel/kernel/fs/fileoperate/fileoperate.c
@@ -26,73 +26,75 @@ SYSCALL_DEFINE3(write, unsigned int, fd, const char __user *, buf,
 #endif
 
 #define KERNEL_FILE "/project/kernel_file"
- 
 
-static char buf[] = "HELLO FANSAIHUA\n"; 
-char buf1[24] = {0}; 
-  
-int   fs_operations(void) 
-{ 
-   struct file *fp; 
-    mm_segment_t old_fs; 
-    loff_t pos; 
+
+static char buf[] = "HELLO FANSAIHUA\n";
+char buf1[24] = {0};
+
+int   fs_operations(void)
+{
+   struct file *fp;
+    mm_segment_t old_fs;
+    loff_t pos;
     size_t f_size = 0;
     size_t r_size = 0;
-    
-    old_fs = get_fs(); 
-    set_fs(KERNEL_DS); 
-    //set_fs(get_ds); //get_ds获得kernel的内存访问地址范围ARM LINUX 4G
-    
-    /*O_CREAT: IF not exist ,Creat it*/
-    fp = filp_open(KERNEL_FILE, O_RDWR | O_CREAT, 0644); 
-    
-   if (IS_ERR(fp)) 
-    { 
-       printk("create file error:%s\n",KERNEL_FILE); 
-       goto ERROR_EXIT;
-    } 
-   
 
-    pos = 4; 
+    old_fs = get_fs();
+/*filp_open等属于系统调用是用户空间的操作， set_fs(KERNEL_DS)让
+filp_open在内核空间也可以使用*/
+    set_fs(KERNEL_DS);
+
+
+    /*O_CREAT: IF not exist ,Creat it*/
+    fp = filp_open(KERNEL_FILE, O_RDWR | O_CREAT, 0644);
+
+   if (IS_ERR(fp))
+    {
+       printk("create file error:%s\n",KERNEL_FILE);
+       goto ERROR_EXIT;
+    }
+
+
+    pos = 4;
 
 ///////////////////////////////////////////////
-/*write*/    
+/*write*/
 /*以下两种方法都可以
 **建议使用vfs_write
 */
 #if 1
-    printk("fp->f_pos %lld\n", fp->f_pos); 
+    printk("fp->f_pos %lld\n", fp->f_pos);
     vfs_write(fp, buf, sizeof(buf), &pos);
 //    vfs_write(fp, buf, sizeof(buf), &fp->f_pos);
-    printk("fp->f_pos %lld\n", fp->f_pos); 
+    printk("fp->f_pos %lld\n", fp->f_pos);
 #else
     fp->f_op->write(fp, (char *)buf, sizeof(buf), &fp->f_pos);
 #endif
 
 /*read*/
-    pos = 0; 
+    pos = 0;
     r_size=vfs_read(fp, buf1, sizeof(buf), &pos);
     if(r_size != sizeof(buf))
         {
             printk("vfs_read error\n");
         }
-    
-    printk("read: %s\n", buf1); 
+
+    printk("read: %s\n", buf1);
 
 /*get file size*/
     f_size = vfs_llseek(fp, 0, SEEK_END);//SEEK_SET为定位到文件头
-    printk(" The file size is %d\n", f_size);
-    
-    filp_close(fp, NULL); 
-    set_fs(old_fs); 
-    
-   return 0; 
+    printk(" The file size is %lu\n", f_size);
+
+    filp_close(fp, NULL);
+    set_fs(old_fs);
+
+   return 0;
   ERROR_EXIT:
     {
-         set_fs(old_fs); 
-          return -1; 
+         set_fs(old_fs);
+          return -1;
     }
-} 
+}
 
 int __init kernel_fs_operate_init(void)
 {
@@ -100,13 +102,13 @@ int __init kernel_fs_operate_init(void)
     return 0;//module_init 必须要RETURN ;不然INSMOD 的时候报错
 }
 
-void __exit hello_exit(void) 
-{ 
+void __exit hello_exit(void)
+{
    //sys_unlink(KERNEL_FILE);
-} 
- 
-module_init(kernel_fs_operate_init); 
-module_exit(hello_exit); 
+}
+
+module_init(kernel_fs_operate_init);
+module_exit(hello_exit);
 
 MODULE_LICENSE("GPL");
 
