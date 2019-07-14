8404d63ae20588135ac90c24a200e3e852ac0d9c
andycodes <brucefan@foxmail.com>
2019-07-14 09:47:13 +0800

    mcu

---
+++ mcu/bsp/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+#include "add.h"
+#include "sub.h"
+
+int main()
+{
+	int a = 10, b = 12;
+	float x = 1.234f, y = 9.876f;
+
+	printf("int a+b IS:%d\n", add_int(a, b));
+	printf("int a-b IS:%d\n", sub_int(a, b));
+	printf("float x+y IS:%f\n", add_float(x, y));
+
+	return 0;
+}
