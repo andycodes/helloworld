6bfc9ec4b7859b7bd6f57a1786413cc872a62d0c
andycodes <brucefan@foxmail.com>
2019-08-17 14:20:43 +0800

    clean2

--- bsp/c_base/configs/bar.h
+++
@@ -1,7 +0,0 @@
-#ifndef BAR_H_
-#define BAR_H_
-
-void bar(int i);
-int hello_bar(void);
-
-#endif
--- bsp/c_base/configs/foo.h
+++
@@ -1,7 +0,0 @@
-#ifndef FOO_H_
-#define FOO_H_
-
-void foo(int i);
-int hello_foo(void);
-
-#endif
