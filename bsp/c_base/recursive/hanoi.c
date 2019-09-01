57abf583810435fcb356fa7b9ae5fdbed585a66e
andycodes <brucefan@foxmail.com>
2019-09-01 13:35:15 +0800

    haono

--- bsp/c_base/recursive/hanoi.c
+++ bsp/c_base/recursive/hanoi.c
@@ -8,24 +8,28 @@
 * @param[in] z 目标塔座
 * @return 无
 */
-void hanoi(int n, char x, char y, char z) {
-if(n == 1) {
-/* 将编号为n 的圆盘从x 移到z */
-printf("%d from %c to %c\n", n, x, z);
-return;
-} else {
-/* 将x 上编号1 至n-1 的圆盘移到y，z 作辅助塔*/
-hanoi(n-1, x, z, y);
-/* 将编号为n 的圆盘从x 移到z */
-printf("%d from %c to %c\n", n, x, z);
-/* 将y 上编号1 至n-1 的圆盘移到z，x 作辅助塔*/
-hanoi(n-1, y, x, z);
+void hanoi(int n, char x, char y, char z)
+{
+	if(n == 1) {
+		/* 将编号为n 的圆盘从x 移到z */
+		printf("%d from %c to %c\n", n, x, z);
+		return;
+	} else {
+		/* 将x 上编号1 至n-1 的圆盘移到y，z 作辅助塔*/
+		hanoi(n-1, x, z, y);
+		/* 将编号为n 的圆盘从x 移到z */
+		printf("%d from %c to %c\n", n, x, z);
+		/* 将y 上编号1 至n-1 的圆盘移到z，x 作辅助塔*/
+		hanoi(n-1, y, x, z);
+	}
 }
-}
-int main() {
-int n;
-scanf("%d", &n);
-printf("%d\n", (1 << n) - 1); /* 总次数*/
-hanoi(n, 'A', 'B', 'C');
-return 0;
+
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	printf("%d\n", (1 << n) - 1); /* 总次数*/
+	hanoi(n, 'A', 'B', 'C');
+	return 0;
 }
