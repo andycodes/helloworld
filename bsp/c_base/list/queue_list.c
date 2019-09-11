c156b82b969019b9aef8edad653956fe29a26f10
andycodes <brucefan@foxmail.com>
2019-09-11 22:33:34 +0800

    queue_list

--- bsp/c_base/list/queue_list.c
+++ bsp/c_base/list/queue_list.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 #include "list.h"
 
 #define QUEUE_HEAD(name) LIST_HEAD(name)
@@ -6,9 +10,9 @@
 typedef struct  {
 	struct list_head queue;
 	int data;
-} Q_NODE_TYPE;
+} Q_ENTRY_TYPE;
 
-typedef struct list_head struct queue_head;
+#define queue_head list_head
 
 static QUEUE_HEAD(queue1);
 
@@ -17,16 +21,47 @@ static inline void queue_push(struct queue_head *new, struct queue_head *head)
     list_add_tail(new,head);
 }
 
-#define queue_front(ptr) list_first_entry(ptr, Q_NODE_TYPE, queue)
+#define queue_front(ptr_queue) list_first_entry(ptr_queue, Q_ENTRY_TYPE, queue)
+
+
+static inline void queue_pop(struct queue_head *head)
+{
+        Q_ENTRY_TYPE  *entry1;
+
+         entry1 = queue_front(head);
+
+         list_del(&entry1->queue);
+}
+
+
+static inline int queue_size(struct queue_head *head)
+{
+    Q_ENTRY_TYPE  *pos;
+    int cnt = 0;
+
+    list_for_each_entry(pos,head,queue)cnt++;
+
+    return cnt;
+}
+
+#define queue_is_empty(queue_head)   list_empty(queue_head)
 
 
 int main(int argc, char *argv[])
 {
-    Q_NODE_TYPE  node1;
-    node1 = (Q_NODE_TYPE *)malloc(sizeof(Q_NODE_TYPE));
-    node1->data = 1;
-    queue_push(&node1->queue,&queue1);
+    Q_ENTRY_TYPE  *entry1;
+    entry1 = (Q_ENTRY_TYPE *)malloc(sizeof(Q_ENTRY_TYPE));
+    entry1->data = 78;
+    queue_push(&entry1->queue,&queue1);
+    printf("queue size %d\n",queue_size(&queue1));
+    printf("queue_is_empty %d\n",queue_is_empty(&queue1));
 
-    node1 = queue_front(&queue1);
+    entry1->data = 99;
+    entry1 = queue_front(&queue1);
+    printf("queue entry1 data %d\n",entry1->data);
+
+    queue_pop(&queue1);
+    printf("queue size %d\n",queue_size(&queue1));
+    printf("queue_is_empty %d\n",queue_is_empty(&queue1));
 }
 
