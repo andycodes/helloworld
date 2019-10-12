938704ac95c1b827e90b00f2e3a7d6ae37679417
andycodes <brucefan@foxmail.com>
2019-10-12 20:16:55 +0800

    link

--- bsp/c_base/algorithm/string/string_lib.c
+++ bsp/c_base/algorithm/string/string_lib.c
@@ -4,6 +4,32 @@
 
 //The strchr() function returns a pointer to the first occurrence of the character c in the string s.
 
+/*
+C语言strrev()函数：字符串逆置（倒序、逆序）
+*/
+char* strrev(char* s)
+{
+    /* h指向s的头部 */
+    char* h = s;
+    char* t = s;
+    char ch;
+
+    /* t指向s的尾部 */
+    while(*t++){};
+    t--;    /* 与t++抵消 */
+    t--;    /* 回跳过结束符'\0' */
+
+    /* 当h和t未重合时，交换它们所指向的字符 */
+    while(h < t)
+    {
+        ch = *h;
+        *h++ = *t;    /* h向尾部移动 */
+        *t-- = ch;    /* t向头部移动 */
+    }
+
+    return(s);
+}
+
 
 size_t strlen(const char *str) {
 	const char *s;
