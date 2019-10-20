bc92a1cf362bb63e120d3c758bb71c9759dde5cc
andycodes <brucefan@foxmail.com>
2019-10-20 21:04:59 +0800

    completion

--- bsp/linuxkernel/kernel/pv/completions/completions.c
+++ bsp/linuxkernel/kernel/pv/completions/completions.c
@@ -2,27 +2,25 @@
 #include<linux/module.h>
 #include<linux/sched.h>
 #include<linux/sem.h>
-#include <linux/kthread.h> 
+#include <linux/kthread.h>
 #include <linux/jiffies.h>
 
- 
+
 struct completion my_completion1;
 struct completion my_completion2;//定义了两个完成量
 
 static struct task_struct * task_one = NULL;
 static struct task_struct * task_two = NULL;
 
- 
+
 int thread_one(void *p)
 {
 	unsigned long timeleft;
 	printk(KERN_ALERT"DRIVER:I AM WAITING FOR SALEMAN CLOSED THE DOOR\n");
-	//wait_for_completion(&my_completion1);//等待完成量completion1
 	reinit_completion(&my_completion1);
-	timeleft = wait_for_completion_timeout(&my_completion1,msecs_to_jiffies(20));
+	timeleft = wait_for_completion_timeout(&my_completion1,msecs_to_jiffies(200));
 	if (!timeleft) {
 		pr_err("[%s]wait_for_completion_timeout\n", __func__);
-		return -1;
 	}
 
 	printk(KERN_ALERT"DRIVER:OK , LET'S GO!NOW~\n");
@@ -34,21 +32,23 @@ int thread_one(void *p)
 
  int thread_two(void *p)
 {
-	printk(KERN_ALERT"SALEMAN:THE DOOR IS CLOSED!\n");
-	complete(&my_completion1);//唤醒完成量completion1
-	printk(KERN_ALERT"SALEMAN:YOU CAN GO NOW!\n");
+	if (!completion_done(&my_completion1)) {
+		complete(&my_completion1);
+		printk(KERN_ALERT"SALEMAN:YOU CAN GO NOW!\n");
+	}
+
 	reinit_completion(&my_completion2);
 	wait_for_completion(&my_completion2);//等待完成量completion2
 	printk(KERN_ALERT"SALEMAN:OK,THE DOOR BE OPENED!\n");
 	return 0;
  }
- 
+
  static int hello_init(void)
  {
 	int err;
 	init_completion(&my_completion1);
 	init_completion(&my_completion2);
-    
+
 	task_one = kthread_create(thread_one, NULL, "task_one");
 	if (IS_ERR(task_one)){
 		pr_err("[%s]Unable to start kernel task_one.\n", __func__);
@@ -70,13 +70,13 @@ int thread_one(void *p)
 
 	return 0;
  }
- 
+
  static void hello_exit(void)
 {
 	if(task_one)kthread_stop(task_one);
 	if(task_two)kthread_stop(task_two);
 }
- 
+
 module_init(hello_init);
 module_exit(hello_exit);
 MODULE_AUTHOR("FANSAIHUA");
