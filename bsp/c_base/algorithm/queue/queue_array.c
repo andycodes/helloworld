fe42da7b61bfd242e57064bd0b2c9bdee71f452e
andycodes <brucefan@foxmail.com>
2019-11-14 22:35:29 +0800

    queue

--- bsp/c_base/algorithm/queue/queue_array.c
+++ bsp/c_base/algorithm/queue/queue_array.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 #include <malloc.h>
 
-/**
- * C 语言: 数组实现的队列，只能存储int数据。
- *
- */
-
+struct queue_load{
+	int data;
+};
 
 struct queue_blk{
 	int count;
-	int arr[0];
+	struct queue_load load[0];
 };
 
 struct queue_blk * create_array_queue(int sz)
 {
 	struct queue_blk * queue;
 
-	queue = (struct queue_blk * )malloc(sizeof(struct queue_blk) + sz*sizeof(int));
+	queue = (struct queue_blk * )malloc(sizeof(struct queue_blk) + sz*sizeof(struct queue_load));
 	if(queue == NULL) {
 		printf(" malloc error!");
 		return  NULL;
@@ -30,37 +28,35 @@ struct queue_blk * create_array_queue(int sz)
 
 void destroy_array_queue(struct queue_blk * queue)
 {
-	if (queue)
-	{
+	if (queue) {
 		free((void*)queue);
 		queue = NULL;
 	}
 }
 
 
-void push(struct queue_blk * queue,int val)
+void push(struct queue_blk * queue,struct queue_load load)
 {
-	queue->arr[queue->count++] = val;
+	queue->load[queue->count++] = load;
 }
 
 
-int front(struct queue_blk * queue)
+struct queue_load *front(struct queue_blk * queue)
 {
-	return queue->arr[0];
+	return &queue->load[0];
 }
 
 
 // 返回并删除“队列开头元素”
-int pop(struct queue_blk * queue)
+void pop(struct queue_blk * queue, struct queue_load *head)
 {
-	int i = 0;;
-	int ret = queue->arr[0];
+	int i = 0;
+
+	*head = queue->load[0];
 
 	queue->count--;
 	while (i++<queue->count)
-		queue->arr[i-1] = queue->arr[i];
-
-	return ret;
+		queue->load[i-1] = queue->load[i];
 }
 
 
@@ -78,33 +74,41 @@ int is_empty(struct queue_blk * queue)
 
 void main()
 {
-	int tmp=0;
 	struct queue_blk *q_test1;
 
-	// 创建“队列”
 	q_test1 = create_array_queue(12);
 
-	// 将10, 20, 30 依次推入队列中
-	push(q_test1,10);
-	push(q_test1,20);
-	push(q_test1,30);
+	struct queue_load load1;
+	struct queue_load load2;
+	struct queue_load *pload;
+
+	load1.data = 10;
+	push(q_test1,load1);
+	load1.data = 20;
+	push(q_test1,load1);
+	load1.data = 30;
+	push(q_test1,load1);
 
 	// 将“队列开头的元素”赋值给tmp，并删除“该元素”
-	tmp = pop(q_test1);
-	printf("tmp=%d\n", tmp);
+	pop(q_test1,&load2);
+	printf("tmp=%d\n", load2.data);
 
 	// 只将“队列开头的元素”赋值给tmp，不删除该元素.
-	tmp = front(q_test1);
-	printf("tmp=%d\n", tmp);
+	pload = front(q_test1);
+	printf("tmp=%d\n", pload->data);
 
-	push(q_test1,40);
+	load1.data = 40;
+	push(q_test1,load1);
 
 	// 打印队列
 	printf("is_empty()=%d\n", is_empty(q_test1));
 	printf("size()=%d\n", size(q_test1));
-	while (!is_empty(q_test1))
-	{
-		printf("%d\n", pop(q_test1));
+
+	while (!is_empty(q_test1)) {
+		struct queue_load tmp_load;
+
+		pop(q_test1,&tmp_load);
+		printf("%d\n", tmp_load.data);
 	}
 
 	// 销毁队列
