e2bbde2363ca8bc86660974e653d464dbb6cb46f
andycodes <brucefan@foxmail.com>
2019-11-17 15:56:00 +0800

    array

--- bsp/c_base/array.c
+++ bsp/c_base/array.c
@@ -47,3 +47,23 @@ void gameOfLife(int** board, int boardSize, int* boardColSize){
     }
 }
 
+
+#include <stdio.h>
+/*
+a+i == p+i
+a[i] == p[i] == *(a+i) == *(p+i)
+a[i][j] == p[i][j] == *(a[i]+j) == *(p[i]+j) == *(*(a+i)+j) == *(*(p+i)+j)
+*/
+int main(){
+    int a[3][4]={0,1,2,3,4,5,6,7,8,9,10,11};
+    int(*p)[4];
+    int i,j;
+    p=a;
+    for(i=0; i<3; i++){
+		for(j=0; j<4; j++)
+			printf("%2d  ",*(*(p+i)+j));
+		printf("\n");
+	}
+	return 0;
+}
+
