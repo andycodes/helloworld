a51f9d2f7e5ea3cd42bfc08aa3b73c03e0f50d88
andycodes <brucefan@foxmail.com>
2019-11-23 11:38:01 +0800

    dfs

--- bsp/c_base/algorithm/tree/tree_binary.c
+++ bsp/c_base/algorithm/tree/tree_binary.c
@@ -100,7 +100,8 @@ AB的两个当前节点都为空 返回null
 都不为空 则将B的val 加到A的val上 返回当前节点
 */
 
-struct TreeNode* mergeTrees(struct TreeNode* t1, struct TreeNode* t2){
+struct TreeNode* mergeTrees(struct TreeNode* t1, struct TreeNode* t2)
+{
         //空节点直接返回
         if(t1 == NULL) return t2;
         if(t2 == NULL) return t1;
@@ -417,5 +418,62 @@ char ** binaryTreePaths(struct TreeNode* root, int* returnSize){
    _binaryTreePaths(root, buf, 0, res, returnSize);
 
    return res;
+}
+
+
+
+/*
+给定两个二叉树，编写一个函数来检验它们是否相同。
+
+如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+
+示例 1:
+
+输入:       1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+输出: true
+示例 2:
+
+输入:      1          1
+          /           \
+         2             2
+
+        [1,2],     [1,null,2]
+
+输出: false
+示例 3:
+
+输入:       1         1
+          / \       / \
+         2   1     1   2
+
+        [1,2,1],   [1,1,2]
+
+输出: false
+
+
+*/
+bool dfs(struct TreeNode* t1, struct TreeNode* t2)
+{
+	if (t1 == NULL && t2 == NULL)
+		return true;
+	else if (t1 == NULL && t2 != NULL)
+		return false;
+	else if (t1 != NULL && t2 == NULL)
+		return false;
+	else if (t1->val != t2->val)
+		return false;
+
+
+	return dfs(t1->left,t2->left) && dfs(t1->right,t2->right);
+}
+
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q){
+	return dfs(p,q);
 }
 
