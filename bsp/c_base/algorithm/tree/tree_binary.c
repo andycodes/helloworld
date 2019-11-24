98e6980107993f3028384e7e3fac1d3cb7f0a15d
andycodes <brucefan@foxmail.com>
2019-11-24 16:26:45 +0800

    dfs

--- bsp/c_base/algorithm/tree/tree_binary.c
+++ bsp/c_base/algorithm/tree/tree_binary.c
@@ -23,12 +23,11 @@ struct TreeNode* invertTree(struct TreeNode* root){
 如果不为空，分别求左子树的深度和右子树的深度，
 取最大的再加1。
 */
-
-int maxDepth(struct TreeNode* root){
+int maxDepth(struct TreeNode* root)
+{
     if(root == NULL)
         return 0;
 
-    //分别计算左子树和右子树的深度
     int leftDepth = maxDepth(root->left) + 1;
     int rightDepth = maxDepth(root->right) + 1;
 
@@ -48,7 +47,8 @@ int maxDepth(struct TreeNode* root){
 深度赋值为最大值。
 */
 
-int minDepth(TreeNode *root) {
+int minDepth(struct TreeNode *root)
+{
         if(root == NULL)
             return false;
         if(root->left == NULL && root->right == NULL)
@@ -72,21 +72,22 @@ int minDepth(TreeNode *root) {
 则返回左子树和右子树深度的最小值。
 */
 
-int minDepth(TreeNode *root) {
-        if(root == nullptr)
+int minDepth(struct TreeNode *root)
+{
+        if(root == NULL)
             return 0;
 
         //判断左子树或右子树是否为空
-        //若左子树为空，则返回右子树的深度，反之返回左子树的深度
-        if(root->left == nullptr)
+        //若左子树为空，则返回右子树的深度，
+        //反之返回左子树的深度
+        if(root->left == NULL)
             return minDepth(root->right) + 1;
-        if(root->right == nullptr)
+        if(root->right == NULL)
             return  minDepth(root->left) + 1;
 
         //如果都不为空，则返回左子树和右子树深度的最小值
         int leftDepth = minDepth(root->left) + 1;
         int rightDepth = minDepth(root->right) + 1;
-
         return leftDepth < rightDepth ? leftDepth: rightDepth;
 }
 
@@ -476,4 +477,98 @@ bool dfs(struct TreeNode* t1, struct TreeNode* t2)
 bool isSameTree(struct TreeNode* p, struct TreeNode* q){
 	return dfs(p,q);
 }
+
+/*
+给定一个二叉树，判断它是否是高度平衡的二叉树。
+
+本题中，一棵高度平衡二叉树定义为：
+
+一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+
+示例 1:
+
+给定二叉树 [3,9,20,null,null,15,7]
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回 true 。
+
+示例 2:
+
+给定二叉树 [1,2,2,3,3,null,null,4,4]
+
+       1
+      / \
+     2   2
+    / \
+   3   3
+  / \
+ 4   4
+返回 false 。
+
+
+
+从底至顶（提前阻断法）
+对二叉树做深度优先遍历DFS，递归过程中：
+终止条件：当DFS越过叶子节点时，返回高度0；
+返回值：
+从底至顶，返回以每个节点root为根节点的子树最大高度(左右子树中最大的高度值加1max(left,right) + 1)；
+当我们发现有一例 左/右子树高度差 ＞ 1 的情况时，代表此树不是平衡树，返回-1；
+当发现不是平衡树时，后面的高度计算都没有意义了，因此一路返回-1，避免后续多余计算。
+最差情况是对树做一遍完整DFS，时间复杂度为 O(N)。
+
+*/
+#define max(a, b) ((a) > (b) ? (a) : (b))
+int  dfs(struct TreeNode* root)
+{
+	if (root == NULL)
+		return 0;
+
+	int left = dfs(root->left);
+	if (left == -1)
+		return -1;
+
+	int right = dfs(root->right);
+	if (right == -1)
+		return -1;
+
+	return abs(left -right) < 2 ? max(left,right) +1 : -1;
+}
+
+bool  isBalanced(struct TreeNode* root){
+	return dfs(root) != -1;
+}
+/*
+从顶至底（暴力法）
+构造一个获取当前节点最大深度的方法 depth() ，通过比较左右子树最大高度差abs(self.depth(root.left) - self.depth(root.right))，来判断以此节点为根节点下是否是二叉平衡树；
+从顶至底DFS，以每个节点为根节点，递归判断是否是平衡二叉树：
+若所有根节点都满足平衡二叉树性质，则返回 True ；
+若其中任何一个节点作为根节点时，不满足平衡二叉树性质，则返回False。
+本方法产生大量重复的节点访问和计算，最差情况下时间复杂度 O(N^2)。
+
+*/
+
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
+int  dfs_depth(struct TreeNode* root)
+{
+	if (root == NULL)
+		return 0;
+
+	return max(dfs_depth(root->left),dfs_depth(root->right)) + 1;
+}
+
+bool  isBalanced(struct TreeNode* root)
+{
+	if (root == NULL)
+		return true;
+
+	return (abs(dfs_depth(root->left) - dfs_depth(root->right)) <= 1) &&
+			(isBalanced(root->left))&&
+			(isBalanced(root->right));
+}
+
 
