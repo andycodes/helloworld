d9ad5389458a7a5a8d15a8e9d31d206721548c69
andycodes <brucefan@foxmail.com>
2019-12-08 08:22:44 +0800

    qsort

--- bsp/c_base/algorithm/search/search_binary.c
+++ bsp/c_base/algorithm/search/search_binary.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+
+
+
 /** 数组元素的类型*/
 typedef int elem_t;
 /**
@@ -24,4 +28,110 @@ int binary_search(const elem_t a[], const int n, const elem_t x) {
         }
     }
     return -(left+1);
+}
+
+
+int binary_find_recur(int a[],int key,int start,int end)
+{
+	int middle=(start+end)/2;
+	if(a[middle]==key)
+		return middle;
+	if(start==middle)
+		return -1;
+        if(key<a[middle])
+		binary_find_recur(a,key,start,middle);
+	else
+		binary_find_recur(a,key,middle,end);
+}
+
+
+int test_binary_find_recur()
+{
+    int a[]={1,2,3,4,5,6,7,8,9,10};
+    printf("%d\n",binary_find_recur(a,10,0,10));
+}
+
+
+
+/*bseach用法:
+void *bsearch(const void *key, const void *base,
+size_t nelem, size_t width, int(*fcmp)(const void *, const *));
+参数：
+key指向所要查找的元素.
+第一个找的关键字。
+第二个：要查找的数组。
+第三个：指定数组中元素的数目。
+第四个：每个元素的长度（以字符为单位）。
+第五个：指向比较函数的指针.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define LEN 5
+
+void arr_init(int num[], int len)
+{
+	int i;
+
+	srand(time(NULL));
+	for(i = 0; i < len; i++){
+		num[i] = rand()%50;
+	}
+
+}
+
+void arr_print(int num[], int len)
+{
+	int i;
+
+	for(i = 0; i < len; i++){
+		printf("%d ", num[i]);
+	}
+	printf("\n");
+
+}
+//int
+int cmp_int(const void *a, const void *b)
+{
+	return *(int *)a - *(int *)b;
+
+}
+
+void test_bsearch(void)
+{
+	int num[LEN];
+	int fd;
+	int *find_num;
+
+	arr_init(num, LEN);
+	arr_print(num, LEN);
+	qsort(num, LEN, sizeof(int), cmp_int);
+	arr_print(num, LEN);
+	printf("enter search num:\n");
+	scanf("%d", &fd);
+	find_num = bsearch(&fd, num, LEN, sizeof(int), cmp_int);
+	(find_num == NULL) ? printf("no find\n") : printf("find the num %d\n", fd);
+}
+
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2) {
+		printf("argc err\n");
+		return -1;
+	}
+
+	switch(atoi(argv[1])) {
+	case 1:
+		test_binary_find_recur();
+	break;
+	case 2:
+		test_bsearch();
+	default:
+	break;
+	}
+
+	return 0;
 }
