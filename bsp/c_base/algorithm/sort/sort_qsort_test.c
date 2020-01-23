5823873e8582dee5534c6f54a6173f5f2e8a21c6
andycodes <brucefan@foxmil.com>
2020-01-23 19:49:52 +0800

    sort

---
+++ bsp/c_base/algorithm/sort/sort_qsort_test.c
@@ -0,0 +1,56 @@
+
+/*
+324. 摆动排序 II
+给定一个无序的数组 nums，将它重新排列成 nums[0] < nums[1] > nums[2] < nums[3]... 的顺序。
+
+示例 1:
+
+输入: nums = [1, 5, 1, 1, 6, 4]
+输出: 一个可能的答案是 [1, 4, 1, 5, 1, 6]
+
+大小数据分2组+小数组降序大数组升序+大小数组互相穿插
+*/
+
+int cmp_int ( const void *a , const void *b){
+        return *(int *)a - *(int *)b;
+}
+
+void wiggleSort(int* nums, int numsSize){
+	if (nums == NULL || numsSize < 2)
+		return;
+
+	qsort(nums, numsSize, sizeof(int), cmp_int);
+
+	int big_count = 0, small_count = 0;
+
+	if (numsSize % 2 == 1) {
+		big_count = numsSize / 2;
+		small_count = numsSize / 2 + 1;
+	} else {
+		big_count = numsSize / 2;
+		small_count = numsSize / 2;
+	}
+
+	int big[big_count];
+	int small[small_count];
+
+	for (int i = 0; i < small_count; i++) {
+		 small[small_count-i-1] = nums[i];
+	}
+
+	for(int i = small_count; i < numsSize; i++){
+            big[i-small_count] = nums[i];
+        }
+
+	int small_index = 0, big_index = big_count-1;
+        for(int i=0;i<numsSize;i++){
+            if(i % 2 == 0){
+                nums[i] = small[small_index];
+                small_index++;
+            }else{
+                nums[i] = big[big_index];
+                big_index--;
+            }
+        }
+}
+
