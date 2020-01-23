be95f598c4237501eaba2b95f4041080117d02ea
andycodes <brucefan@foxmil.com>
2020-01-23 21:37:32 +0800

    math

--- bsp/c_base/algorithm/math/math.c
+++ bsp/c_base/algorithm/math/math.c
@@ -57,3 +57,41 @@ int* corpFlightBookings(int** bookings, int bookingsSize, int* bookingsColSize, 
 	return diff;
 }
 
+/*
+1010. 总持续时间可被 60 整除的歌曲
+在歌曲列表中，第 i 首歌曲的持续时间为 time[i] 秒。
+
+返回其总持续时间（以秒为单位）可被 60 整除的歌曲对的数量。形式上，我们希望索引的数字  i < j 且有 (time[i] + time[j]) % 60 == 0。
+
+
+
+示例 1：
+
+输入：[30,20,150,100,40]
+输出：3
+解释：这三对的总持续时间可被 60 整数：
+(time[0] = 30, time[2] = 150): 总持续时间 180
+(time[1] = 20, time[3] = 100): 总持续时间 120
+(time[1] = 20, time[4] = 40): 总持续时间 60
+
+*/
+
+int numPairsDivisibleBy60(int* time, int timeSize){
+	int result = 0;
+	int mod[60];
+
+	memset((void*)mod, 0, sizeof(mod));
+
+	for(int i = 0; i < timeSize; i++) {
+		mod[time[i] % 60]++;
+	}
+
+	for (int i = 1; i < 30; i++) {
+		result += mod[i] * mod[60 - i];
+	}
+
+	result += (mod[0] * (mod[0] - 1) + mod[30] * (mod[30] - 1)) / 2;
+
+	return result;
+}
+
