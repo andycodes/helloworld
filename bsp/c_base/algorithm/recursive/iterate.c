152269460bfc27f6292c51c55b87adf1d3b6accd
andycodes <brucefan@foxmil.com>
2020-01-24 08:48:09 +0800

    iterate

--- bsp/c_base/algorithm/recursive/iterate.c
+++ bsp/c_base/algorithm/recursive/iterate.c
@@ -115,3 +115,71 @@ char ** readBinaryWatch(int num, int* returnSize){
 	return res;
 }
 
+/*
+456. 132模式
+给定一个整数序列：a1, a2, ..., an，一个132模式的
+子序列 ai, aj, ak 被定义为：当 i < j < k 时，ai < ak < aj。
+设计一个算法，当给定有 n 个数字的序列时，
+验证这个序列中是否含有132模式的子序列。
+
+注意：n 的值小于15000。
+
+示例1:
+
+输入: [1, 2, 3, 4]
+
+输出: False
+
+解释: 序列中不存在132模式的子序列。
+
+*/
+
+bool find132pattern(int* nums, int numsSize) {
+
+    if (nums == NULL || numsSize < 3 || numsSize >= 15000)
+        return false;
+
+	int min[numsSize];
+
+	min[0] = nums[0];
+	for (int i = 1; i < numsSize; i++) {
+		min[i] = fmin(min[i - 1], nums[i]);
+	}
+
+	for (int j = 1; j < numsSize; j++) {
+		if (min[j] >=  nums[j])
+			continue;
+		for (int k = j + 1; k < numsSize; k++) {
+			if (min[j] < nums[k] && nums[k] < nums[j]) {
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+
+bool find132pattern(int* nums, int numsSize){
+
+	if (nums == NULL || numsSize <= 0 || numsSize >= 15000)
+		return false;
+
+	int min = nums[0];
+
+	for (int j = 1; j < numsSize; j++) {
+		if (nums[j] > min) {
+			for (int k = j + 1; k < numsSize; k++) {
+				if (min < nums[k] && nums[k] < nums[j]) {
+					return true;
+				}
+			}
+		} else {
+			min = nums[j];
+		}
+	}
+
+	return false;
+}
+
+
