dd84c18af3aed06b922a9e9923e286e4039ef26d
andycodes <brucefan@foxmil.com>
2020-02-03 11:13:21 +0800

    point

--- bsp/c_base/algorithm/point/point.c
+++ bsp/c_base/algorithm/point/point.c
@@ -154,3 +154,59 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
+
+/*
+11. 盛最多水的容器
+给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中
+的一个点 (i, ai) 。在坐标内画 n 条垂直线，
+垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，
+使得它们与 x 轴共同构成的容器可以容纳最多的水。
+
+说明：你不能倾斜容器，且 n 的值至少为 2。
+
+*/
+int maxArea(int* height, int heightSize){
+	if (height == NULL || heightSize <= 0) {
+		return 0;
+	}
+
+	int area = INT_MIN;
+	for(int i = 1; i < heightSize; i++) {
+		for (int j = 0; j < i; j++) {
+			area = fmax(area, fmin(height[i], height[j]) * (i - j));
+		}
+	}
+
+	return area;
+}
+/*
+双指针法
+最初我们考虑由最外围两条线段构成的区域。
+现在，为了使面积最大化，
+我们需要考虑更长的两条线段之间的区域。
+如果我们试图将指向较长线段的指针向内侧移动，
+矩形区域的面积将受限于较短的线段而不会获得任何增加。
+但是，在同样的条件下，
+移动指向较短线段的指针尽管造成了矩形宽度的减小，
+但却可能会有助于面积的增大。因为移动较短线段的
+指针会得到一条相对较长的线段，
+这可以克服由宽度减小而引起的面积减小。
+*/
+int maxArea(int* height, int heightSize){
+	if (height == NULL || heightSize <= 0) {
+		return 0;
+	}
+
+	int area = INT_MIN;
+	int l = 0, r = heightSize - 1;
+        while (l < r) {
+            area = fmax(area, fmin(height[l], height[r]) * (r - l));
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return area;
+}
+
+
