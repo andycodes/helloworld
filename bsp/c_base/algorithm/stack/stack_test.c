7888dbbbe54d9ed47ea772af0bb46de81aacaf83
andycodes <brucefan@foxmil.com>
2020-02-19 22:02:58 +0800

    stack

--- bsp/c_base/algorithm/stack/stack_test.c
+++ bsp/c_base/algorithm/stack/stack_test.c
@@ -381,4 +381,96 @@ char * removeDuplicates(char * s, int k){
 	return out;
 }
 
+/*
+给定 pushed 和 popped 两个序列，每个序列中的 值都不重复，只有当它们可能是在最初空栈上进行的推入 push 和弹出 pop 操作序列的结果时，返回 true；否则，返回 false 。
 
+
+
+示例 1：
+
+输入：pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+输出：true
+解释：我们可以按以下顺序执行：
+push(1), push(2), push(3), push(4), pop() -> 4,
+push(5), pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
+示例 2：
+
+输入：pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
+输出：false
+解释：1 不能在 2 之前弹出。
+
+*/
+/*
+方法一： 贪心
+思路
+
+所有的元素一定是按顺序 push 进去的，重要的是怎么 pop 出来？
+
+假设当前栈顶元素值为 2，同时对应的 popped 序列中下一个要 pop 的值也为 2，那就必须立刻把这个值 pop 出来。因为之后的 push 都会让栈顶元素变成不同于 2 的其他值，这样再 pop 出来的数 popped 序列就不对应了。
+
+算法
+
+将 pushed 队列中的每个数都 push 到栈中，同时检查这个数是不是 popped 序列中下一个要 pop 的值，如果是就把它 pop 出来。
+
+最后，检查不是所有的该 pop 出来的值都是 pop 出来了。
+
+*/
+
+bool validateStackSequences(int* pushed, int pushedSize,
+	int* popped, int poppedSize)
+{
+	if (pushed == NULL || popped == NULL) {
+		return false;
+	}
+
+	struct astack *stack = astack_init(pushedSize);
+	int j = 0;
+	for (int i = 0; i < pushedSize; i++) {
+		struct astack_load load;
+		load.data = pushed[i];
+		astack_push(stack, load);
+
+		while(!astack_empty(stack) && j < pushedSize) {
+			struct astack_load load = astack_top(stack);
+			if (load.data == popped[j]) {
+				astack_pop(stack);
+				j++;
+			} else {
+				break;
+			}
+		}
+	}
+
+	return j == pushedSize;
+}
+/*
+解法一：
+使用数组来实现栈的作用，模拟栈的出栈入栈操作。size表示栈的大小，size-1就是栈顶的位置了。
+
+注意：用数组来实现栈，虽然存取更快，但多数情况下其实不推荐。特别是数组pushed可能特别大的时候，那作为栈的数组stack也会特别大。但实际上同时存在栈中的元素往往不是特别多，这就存在很大的浪费了。
+
+*/
+bool validateStackSequences(int* pushed, int pushedSize,
+	int* popped, int poppedSize)
+{
+	if (pushed == NULL || popped == NULL) {
+		return false;
+	}
+
+    if (pushedSize == 0 && poppedSize == 0)
+        return true;
+    else if(pushedSize != poppedSize)
+        return false;
+
+	int stack[pushedSize];
+
+	int size = 0;
+	for (int i = 0, j = 0; i < pushedSize; i++) {
+		stack[size++] = pushed[i];
+		while(size != 0 && stack[size -1] == popped[j]) {
+			size--;j++;
+		}
+	}
+
+	return size ==0;
+}
