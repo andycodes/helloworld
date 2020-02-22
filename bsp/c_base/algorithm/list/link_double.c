86aaee77caf2a7c000733f564a26ce63d1912ef5
andycodes <brucefan@foxmil.com>
2020-02-22 14:07:28 +0800

    link

--- bsp/c_base/algorithm/list/link_double.c
+++ bsp/c_base/algorithm/list/link_double.c
@@ -1,141 +1,57 @@
-
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
-/**
- * C 语言实现的双向链表，能存储任意数据。
- *
- */
-// 双向链表节点
-typedef struct tag_node
-{
-	struct tag_node *prev;
-	struct tag_node *next;
-	void* p;
-}node;
+struct ListNode {
+	struct ListNode *prev;
+	struct ListNode *next;
+	int val;
+	int *p;
+};
 
-// 表头。注意，表头不存放元素值！！！
-static node *phead=NULL;
-// 节点个数。
-static int  count=0;
+typedef  struct DoubleLink {
+	int  cnt;
+	int  size;
+	struct ListNode *head;
+}DOUBLELIST;
+
 
 // 新建“节点”。成功，返回节点指针；否则，返回NULL。
-static node* create_node(void *pval)
+struct ListNode* dlink_new_node(int value)
 {
-	node *pnode=NULL;
-	pnode = (node *)malloc(sizeof(node));
-	if (!pnode)
-	{
+	struct ListNode *newNode = NULL;
+	newNode = (struct ListNode *)malloc(sizeof(struct ListNode));
+	if (newNode == NULL) {
 		printf("create node error!\n");
 		return NULL;
 	}
-	// 默认的，pnode的前一节点和后一节点都指向它自身
-	pnode->prev = pnode->next = pnode;
-	// 节点的值为pval
-	pnode->p = pval;
 
-	return pnode;
+	// 默认的，newNode的前一节点和后一节点都指向它自身
+	newNode->prev = newNode->next = newNode;
+	// 节点的值为value
+	newNode->value = value;
+
+	return newNode;
 }
 
-// 新建“双向链表”。成功，返回0；否则，返回-1。
-int create_dlink()
+
+struct DoubleLink * dlink_init(int size)
 {
-	// 创建表头
-	phead = create_node(NULL);
-	if (!phead)
-		return -1;
-
-	// 设置“节点个数”为0
-	count = 0;
-
-	return 0;
+	struct DoubleLink * dlink = (struct DoubleLink *)malloc(sizeof(struct DoubleLink));
+	dlink->head = dlink_new_node(-1);
+	dlink->cnt = 0;
+	dlink->size = size;
+	return dlink;
 }
 
-// “双向链表是否为空”
-int dlink_is_empty()
-{
-	return count == 0;
-}
 
-// 返回“双向链表的大小”
-int dlink_size() {
-	return count;
-}
-
-// 获取“双向链表中第index位置的节点”
-static node* get_node(int index)
-{
-	if (index<0 || index>=count)
-	{
-		printf("%s failed! index out of bound!\n", __func__);
-		return NULL;
-	}
-
-	// 正向查找
-	if (index <= (count/2))
-	{
-		int i=0;
-		node *pnode=phead->next;
-		while ((i++) < index)
-			pnode = pnode->next;
-
-		return pnode;
-	}
-
-	// 反向查找
-	int j=0;
-	int rindex = count - index - 1;
-	node *rnode=phead->prev;
-	while ((j++) < rindex)
-		rnode = rnode->prev;
-
-	return rnode;
-}
-
-// 获取“第一个节点”
-static node* get_first_node()
-{
-	return get_node(0);
-}
-
-// 获取“最后一个节点”
-static node* get_last_node()
-{
-	return get_node(count-1);
-}
-
-// 获取“双向链表中第index位置的元素”。成功，返回节点值；否则，返回-1。
-void* dlink_get(int index)
-{
-	node *pindex=get_node(index);
-	if (!pindex)
-	{
-		printf("%s failed!\n", __func__);
-		return NULL;
-	}
-
-	return pindex->p;
-
-}
-
-// 获取“双向链表中第1个元素的值”
-void* dlink_get_first()
-{
-	return dlink_get(0);
-}
-
-// 获取“双向链表中最后1个元素的值”
-void* dlink_get_last()
-{
-	return dlink_get(count-1);
-}
-
-// 将“pval”插入到index位置。成功，返回0；否则，返回-1。
-int dlink_insert(int index, void* pval)
+// 将“value”插入到index位置。成功，返回0；否则，返回-1。
+int dlink_insert(struct DoubleLink *obj, int index, int value)
 {
 	// 插入表头
 	if (index==0)
-		return dlink_insert_first(pval);
+		return dlink_insert_first(value);
 
 	// 获取要插入的位置对应的节点
 	node *pindex=get_node(index);
@@ -143,7 +59,7 @@ int dlink_insert(int index, void* pval)
 		return -1;
 
 	// 创建“节点”
-	node *pnode=create_node(pval);
+	node *pnode=create_node(value);
 	if (!pnode)
 		return -1;
 
@@ -152,92 +68,195 @@ int dlink_insert(int index, void* pval)
 	pindex->prev->next = pnode;
 	pindex->prev = pnode;
 	// 节点个数+1
-	count++;
+	obj->cnt++;
 
 	return 0;
 }
 
-// 将“pval”插入到表头位置
-int dlink_insert_first(void *pval)
-{
-	node *pnode=create_node(pval);
-	if (!pnode)
-		return -1;
 
-	pnode->prev = phead;
-	pnode->next = phead->next;
-	phead->next->prev = pnode;
-	phead->next = pnode;
-	count++;
-	return 0;
+// 将“value”插入到表头位置
+void dlink_push_head(struct DoubleLink *obj, int value)
+{
+	if(obj->cnt >= obj->size)
+		return;
+
+	struct ListNode *head = obj->head;
+	struct ListNode *newNode = dlink_new_node(value);
+	if (newNode == NULL)
+		return;
+
+	newNode->prev = head;
+	newNode->next = head->next;
+	head->next->prev = newNode;
+	head->next = newNode;
 }
 
-// 将“pval”插入到末尾位置
-int dlink_append_last(void *pval)
-{
-	node *pnode=create_node(pval);
-	if (!pnode)
-		return -1;
 
-	pnode->next = phead;
-	pnode->prev = phead->prev;
-	phead->prev->next = pnode;
-	phead->prev = pnode;
-	count++;
-	return 0;
+void dlink_push_last(struct DoubleLink* obj, int value)
+{
+	if(obj->cnt  >= obj->size)
+		return;
+	struct ListNode *head = obj->head;
+	struct ListNode *newNode = dlink_new_node(value);
+	if (newNode == NULL)
+		return;
+
+	newNode->next = head;
+	newNode->prev = head->prev;
+	head->prev->next = newNode;
+	head->prev = newNode;
+	obj->cnt++;
 }
 
-// 删除“双向链表中index位置的节点”。成功，返回0；否则，返回-1。
-int dlink_delete(int index)
+// 获取“双向链表中第index位置的节点”
+struct ListNode *dlink_get_node(struct DoubleLink* obj, int index)
 {
-	node *pindex=get_node(index);
+	struct ListNode *head = obj->head;
+	if (index < 0 || index >= obj->size) {
+		printf("%s failed! the index in out of bound!\n", __func__);
+		return NULL;
+	}
+
+	// 正向查找
+	if (index <= (obj->size/2)) {
+		int i=0;
+		node *pnode = head->next;
+		while ((i++) < index)
+			pnode = pnode->next;
+
+//		printf("%s %d i=%d, pnode->value=%d\n",
+//				__func__, __LINE__, i, pnode->value);
+		return pnode;
+	}
+
+	// 反向查找
+	int j=0;
+	int rindex = obj->size - index - 1;
+	node *rnode = head->prev;
+	while ((j++) < rindex)
+		rnode = rnode->prev;
+
+//	printf("%s %d j=%d, rnode->value=%d\n",
+//			__func__, __LINE__, j, rnode->value);
+	return rnode;
+}
+
+
+// 获取“双向链表中第index位置的元素的值”。成功，返回节点值；否则，返回-1。
+int dlink_get_value(struct DoubleLink* obj, int index)
+{
+	node *pindex = dlink_get_node(obj, index);
 	if (!pindex)
 	{
-		printf("%s failed! the index in out of bound!\n", __func__);
+		printf("%s failed!\n", __func__);
 		return -1;
 	}
 
-	pindex->next->prev = pindex->prev;
-	pindex->prev->next = pindex->next;
-	free(pindex);
-	count--;
+	return pindex->value;
 
-	return 0;
 }
 
-// 删除第一个节点
-int dlink_delete_first()
+
+int dlink_get_first(struct DoubleLink* obj)
 {
-	return dlink_delete(0);
+	return dlink_get_value(0);
 }
 
-// 删除组后一个节点
-int dlink_delete_last()
+
+int dlink_get_last(struct DoubleLink* obj)
 {
-	return dlink_delete(count-1);
+	return dlink_get_value(obj->cnt -1);
 }
 
-// 撤销“双向链表”。成功，返回0；否则，返回-1。
-int destroy_dlink()
+
+void dlink_delete(struct DoubleLink* obj, int index)
 {
-	if (!phead)
+	struct ListNode *head = obj->head;
+	struct ListNode *entry = dlink_get_node(head, index);
+	if (entry == NULL) {
+		printf("%s failed! the index in out of bound!\n", __func__);
+		return;
+	}
+
+	entry->next->prev = entry->prev;
+	entry->prev->next = entry->next;
+	free(entry);
+	obj->cnt--;
+}
+
+
+void dlink_del_first(struct DoubleLink* obj)
+{
+	dlink_delete(obj->head, 0);
+}
+
+
+bool dlink_del_last(struct DoubleLink* obj)
+{
+    return dlink_delete(obj->head, obj->cnt -1);
+}
+
+
+int dlink_destroy(struct DoubleLink* obj)
+{
+	struct ListNode *head = obj->head;
+	if (!head)
 	{
 		printf("%s failed! dlink is null!\n", __func__);
 		return -1;
 	}
 
-	node *pnode=phead->next;
-	node *ptmp=NULL;
-	while(pnode != phead)
+	struct ListNode *entry=head->next;
+	struct ListNode *ptmp=NULL;
+	while(entry != head)
 	{
-		ptmp = pnode;
-		pnode = pnode->next;
+		ptmp = entry;
+		entry = entry->next;
 		free(ptmp);
 	}
 
-	free(phead);
-	phead = NULL;
-	count = 0;
-
+	free(head);
+	head = NULL;
+	free(obj)
+	obj = NULL;
 	return 0;
 }
+
+
+bool dlink_empty(struct DoubleLink* obj)
+{
+    return obj->cnt == 0;
+}
+
+
+bool dlink_full(struct DoubleLink* obj)
+{
+    return obj->cnt == obj->size;
+}
+
+int dlink_size(struct DoubleLink* obj)
+{
+	return obj->cnt;
+}
+
+
+// 打印“双向链表”
+void dlink_print(struct DoubleLink* obj)
+{
+	if (obj->cnt==0 || (!obj))
+	{
+		printf("stack is Empty\n");
+		return ;
+	}
+
+	struct ListNode *head = obj->head;
+
+	printf("stack size()=%d\n", obj->cnt);
+	node *pnode=head->next;
+	while(pnode != head)
+	{
+		printf("%d\n", pnode->value);
+		pnode = pnode->next;
+	}
+}
+
