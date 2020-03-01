ebc3b71e7d4115aacb5b92234951a275d9c7d9b5
andycodes <brucefan@foxmail.com>
2020-03-01 20:24:46 +0800

    dp

--- bsp/c_base/algorithm/dp/dp.c
+++ bsp/c_base/algorithm/dp/dp.c
@@ -520,8 +520,7 @@ int[][] dp = new int[100][100];
 
 
 /*
-dp[i][j] = dp[i - 1][j]* dp[i][j - 1]) ;
-
+dp[i][j] = dp[i - 1][j]+ dp[i][j - 1] ;
 */
 
 int uniquePaths(int m, int n){
@@ -577,7 +576,51 @@ int uniquePaths(int m, int n) {
     return (int) res;
 }
 
+/*
+63. 不同路径 II
+难度中等234
+一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为"Start" ）。
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为"Finish"）。
+现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
 
+网格中的障碍物和空位置分别用 1 和 0 来表示。
+说明：m 和 n 的值均不超过 100。
+示例 1:
+输入:
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+输出: 2
+解释:
+3x3 网格的正中间有一个障碍物。
+从左上角到右下角一共有 2 条不同的路径：
+1. 向右 -> 向右 -> 向下 -> 向下
+2. 向下 -> 向下 -> 向右 -> 向右
+
+*/
+int uniquePathsWithObstacles(
+int** obstacleGrid, int obstacleGridSize, int* obstacleGridColSize)
+{
+	int m = obstacleGridSize;
+	int n = obstacleGridColSize[0];
+	int dp[n];
+	memset(dp, 0, sizeof(dp));
+	dp[0] = 1;
+
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			if (obstacleGrid[i][j] == 1) {
+				dp[j] = 0;
+			} else if (j > 0) {
+				dp[j] += dp[j - 1];
+			}
+		}
+	}
+
+	return dp[n -1];
+}
 
 
 
