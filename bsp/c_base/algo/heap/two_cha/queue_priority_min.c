2960334ce5dc2a9668a93d9455b7ca0da7113fda
andycodes <brucefan@foxmail.com>
2020-03-18 22:29:08 +0800

    heap

--- bsp/c_base/algo/heap/two_cha/queue_priority_min.c
+++ bsp/c_base/algo/heap/two_cha/queue_priority_min.c
@@ -1,85 +1,107 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-
-#define Error( Str )        FatalError( Str )
-#define FatalError( Str )   fprintf( stderr, "%s\n", Str ), exit( 1 )
+#define INT_MAX ((int)(~0U >> 1))
+#define INT_MIN (-INT_MAX - 1)
 
 typedef int ElementType;
 
-struct HeapStruct;
-typedef struct HeapStruct *PriorityQueue;
-
-
-#define MinPQSize (10)
-#define MinData (-32767)
-
-struct HeapStruct
+struct PriorityQueue
 {
-	int Capacity;
-	int Size;
-	ElementType *Elements;
+	int capacity;
+	int size;
+	ElementType Elements[0];
 };
 
-PriorityQueue Initialize( int MaxElements )
+#if 0
+struct PriorityQueue * pqueue_init(int MaxElements)
 {
-	PriorityQueue H;
+	struct PriorityQueue * H;
 
-	if ( MaxElements < MinPQSize )
-		Error( "Priority queue size is too small!" );
-
-	H = malloc( sizeof( struct HeapStruct ) );
+	H = malloc( sizeof( struct PriorityQueue ) );
 	if ( H == NULL )
-		FatalError( "Out of space!!!" );
+		printf( "Out of space!!!" );
 
 	/* Allocate the array plus one extra for sentinel */
 	H->Elements = malloc( ( MaxElements + 1 )
 		* sizeof( ElementType ) );
 	if ( H->Elements == NULL )
-		FatalError( "Out of space!!!" );
+		printf( "Out of space!!!" );
 
-	H->Capacity = MaxElements;
-	H->Size = 0;
-	H->Elements[ 0 ] = MinData;
+	H->capacity = MaxElements;
+	H->size = 0;
+	H->Elements[ 0 ] = INT_MIN;
+
+	return H;
+}
+#endif
+
+struct PriorityQueue * pqueue_init(int MaxElements)
+{
+	struct PriorityQueue * H;
+
+	/* Allocate the array plus one extra for sentinel */
+	H = malloc(sizeof(struct PriorityQueue) + (MaxElements + 1)* sizeof( ElementType ));
+	if ( H == NULL )
+		printf( "Out of space!!!" );
+
+	H->capacity = MaxElements;
+	H->size = 0;
+	H->Elements[0] = INT_MIN;
 
 	return H;
 }
 
+
+int IsFull( struct PriorityQueue * H )
+{
+	return H->size == H->capacity;
+}
+
+
+int
+IsEmpty( struct PriorityQueue * H )
+{
+	return H->size == 0;
+}
+
+
 /* H->Elements[ 0 ] is a sentinel */
 void
-Insert( ElementType X, PriorityQueue H )
+Insert( ElementType X, struct PriorityQueue * H )
 {
 	int i;
 
 	if ( IsFull( H ) )
 	{
-		Error( "Priority queue is full" );
+		printf( "Priority queue is full" );
 		return ;
 	}
-	for ( i = ++H->Size; H->Elements[ i / 2 ] > X; i /= 2) /* The new element is percolated up the heap  */
+	for ( i = ++H->size; H->Elements[ i / 2 ] > X; i /= 2) /* The new element is percolated up the heap  */
 		H->Elements[ i ] = H->Elements[ i / 2 ];           /* until the correct location is found */
 	H->Elements[ i ] = X;
 }
 
 ElementType
-DeleteMin( PriorityQueue H )
+DeleteMin( struct PriorityQueue * H )
 {
 	int i, Child;
 	ElementType MinElement, LastElement;
 
 	if ( IsEmpty( H ) )
 	{
-		Error( "Priority queue is empty!" );
+		printf( "Priority queue is empty!" );
 		return H->Elements[ 0 ];
 	}
 	MinElement = H->Elements[ 1 ];
-	LastElement = H->Elements[ H->Size-- ];
+	LastElement = H->Elements[ H->size-- ];
 
-	for ( i = 1; i * 2 <= H->Size; i = Child )
+	for ( i = 1; i * 2 <= H->size; i = Child )
 	{
 		/* Find smaller child */
 		Child = i * 2;
-		if ( Child != H->Size && H->Elements[ Child + 1 ]
+		if ( Child != H->size && H->Elements[ Child + 1 ]
 								<H->Elements[ Child ] )
 			Child++;
 
@@ -94,34 +116,23 @@ DeleteMin( PriorityQueue H )
 }
 
 void
-MakeEmpty( PriorityQueue H )
+MakeEmpty( struct PriorityQueue * H )
 {
-	H->Size = 0;
+	H->size = 0;
 }
 
 ElementType
-FindMin( PriorityQueue H )
+FindMin( struct PriorityQueue * H )
 {
 	if( !IsEmpty( H ) )
 		return H->Elements[ 1 ];
-	Error( "Priority Queue is Empty" );
+	printf( "Priority Queue is Empty" );
 	return H->Elements[ 0 ];
 }
 
-int
-IsEmpty( PriorityQueue H )
-{
-	return H->Size == 0;
-}
-
-int
-IsFull( PriorityQueue H )
-{
-	return H->Size == H->Capacity;
-}
 
 void
-Destroy( PriorityQueue H )
+Destroy( struct PriorityQueue * H )
 {
 	free( H->Elements );
 	free( H );
@@ -131,7 +142,7 @@ Destroy( PriorityQueue H )
 int
 main()
 {
-	PriorityQueue H = Initialize( 50 );
+	struct PriorityQueue * H = pqueue_init( 50 );
 	int ar[] = { 32, 21, 16, 24, 31, 19, 68, 65, 26, 13 };
 	int i;
 	for ( i = 0; i < 10; i++ )
