f7ab0fbd7e8af61ce1b0ae2efc6321364fe30081
andycodes <brucefan@foxmail.com>
2020-03-22 09:34:34 +0800

    heap

--- bsp/c_base/algo/heap/two_cha/heap_min.c
+++ bsp/c_base/algo/heap/two_cha/heap_min.c
@@ -1,38 +1,24 @@
-/**
-优先队列---二叉堆(最小堆)
-最小堆：根结点的键值是所有堆结点键值中最小者，
-且每个结点的值都比其孩子的值小。
-它是一颗完全二叉树,它可以是空
-树中结点的值总是不大于其孩子结点的值
-每一个结点的子树也是一个堆
-
-最小堆的优点在于它的动态可维护性。
-
-当前实现是存在重复数
-
- */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-typedef long int HEAP_DTYPE;
-
-#define LENGTH(a) ( (sizeof(a)) / (sizeof(a[0])) )
-
+struct Entry {
+	int key;
+};
 
 typedef struct minHeap{
-	long int cnt;
-	long int size;
-	HEAP_DTYPE heap[0];
+	int cnt;
+	int size;
+	struct Entry heap[0];
 }minHeap;
 
 
-int minHeapGetIndex(minHeap *mh, HEAP_DTYPE data)
+int minHeapGetIndex(minHeap *mh, int key)
 {
-	long int i=0;
+	int i=0;
 
-	for(i=0; i<mh->cnt; i++)
-		if (data==mh->heap[i])
+	for(i = 0; i < mh->cnt; i++)
+		if (key == mh->heap[i].key)
 			return i;
 
 	return -1;
@@ -51,41 +37,28 @@ bool minHeapisFull(minHeap *mh)
 }
 
 
-long int minHeapGetSize(minHeap *mh)
+int minHeapGetSize(minHeap *mh)
 {
 	return mh->cnt;
 }
 
 
-/*
- * 最小堆的向下调整算法
- *
- * 注：数组实现的堆中，
- 第N个节点的
- 左孩子的索引值是(2N+1)，
- 右孩子的索引是(2N+2)。
- *
- * 参数说明：
- *     start -- 被下调节点的起始位置(一般为0，表示从第1个开始)
- *     end   -- 截至范围(一般为数组中最后一个元素的索引)
- */
-static void minheap_filterdown(minHeap *mh, long int start, long int end)
+static void minheap_filterdown(minHeap *mh, int start, int end)
 {
-	long int curNode = start;
-	long int left = 2*curNode + 1;
-	HEAP_DTYPE tmp = mh->heap[curNode];
+	int curNode = start;
+	int leftChild = 2*curNode + 1;
+	struct Entry tmp = mh->heap[curNode];
 
-	while(left <= end) {
-		// "left"是左孩子，"left+1"是右孩子
-		if(left < end && mh->heap[left] > mh->heap[left+1])
-			left++;		// 左右两孩子中选择较小者，即mh->heap[left+1]
+	while(leftChild <= end) {
+		if(leftChild < end && mh->heap[leftChild].key > mh->heap[leftChild+1].key)
+			leftChild++;
 
-		if(tmp <= mh->heap[left])
-			break;		//调整结束
+		if(tmp.key <= mh->heap[leftChild].key)
+			break;
 		else {
-			mh->heap[curNode] = mh->heap[left];
-			curNode = left;
-			left = 2*left + 1;
+			mh->heap[curNode] = mh->heap[leftChild];
+			curNode = leftChild;
+			leftChild = 2*leftChild + 1;
 		}
 	}
 
@@ -93,12 +66,9 @@ static void minheap_filterdown(minHeap *mh, long int start, long int end)
 }
 
 
-/*
- * 删除最小堆中的data
- */
-int minheap_remove(minHeap *mh, long int data)
+int minheap_remove(minHeap *mh, int data)
 {
-	long int index;
+	int index;
 	if(mh->cnt == 0)
 		return  -1;
 
@@ -106,68 +76,49 @@ int minheap_remove(minHeap *mh, long int data)
 	if (index== -1)
 		return -1;
 
-	mh->heap[index] = mh->heap[--mh->cnt];		// 用最后元素填补
-	// 从index号位置开始自上向下调整为最小堆
+	mh->heap[index] = mh->heap[--mh->cnt];
 	minheap_filterdown(mh, index, mh->cnt-1);
 
 	return 0;
 }
 
 
-long int minHeapPop(minHeap *mh)
+struct Entry  minheap_pop(minHeap *mh)
 {
-	HEAP_DTYPE min;
-
-	if(minHeapisEmpty(mh))
-		return 0;
+	struct Entry min;
 
 	min = mh->heap[0];
-	minheap_remove(mh, min);
+	minheap_remove(mh, min.key);
 	return min;
 }
 
 
-/*
- * 最小堆的向上调整算法(从start开始向上直到0，调整堆)
- *
- * 参数说明：
- *     start -- 被上调节点的起始位置
- (一般为数组中最后一个元素的索引)
- */
-static void filter_up(minHeap *mh, long int start)
+static void filter_up(minHeap *mh, int start)
 {
-	long int curNode = start;
-	long int root = (curNode - 1)/2;		// 父(parent)结点的位置
-	HEAP_DTYPE tmp = mh->heap[curNode];
+	int curNode = start;
+	int parent = (curNode - 1)/2;
+	struct Entry tmp = mh->heap[curNode];
 
 	while(curNode > 0) {
-		if(mh->heap[root] <= tmp)
+		if(mh->heap[parent].key <= tmp.key)
 			break;
 		else {
-			mh->heap[curNode] = mh->heap[root];
-			curNode = root;
-			root = (root-1)/2;
+			mh->heap[curNode] = mh->heap[parent];
+			curNode = parent;
+			parent = (parent-1)/2;
 		}
 	}
 
 	mh->heap[curNode] = tmp;
 }
 
-/*
-插入操作一般使用的策略叫做上滤（percolate up）：
-新元素在堆中上滤直到找出正确的位置
-（设堆为H,待插入的元素为X,首先在size+1的位置建立一个空穴，
-然后比较X和空穴的父亲的大小，
-把“大的父亲”换下来，以此推进，
-最后把X放到合适的位置）。
-*/
-int minHeapPush(minHeap *mh, long int data)
+int minheap_push(minHeap *mh, struct Entry node)
 {
 	if(minHeapisFull(mh)) {
 		return -1;
 	}
 
-	mh->heap[mh->cnt] = data;// 将"数组"插在表尾
+	mh->heap[mh->cnt] = node;
 	filter_up(mh, mh->cnt);
 	mh->cnt++;
 
@@ -175,24 +126,17 @@ int minHeapPush(minHeap *mh, long int data)
 }
 
 
-/*
- * 打印二叉堆
- *
- * 返回值：
- *     0，表示成功
- *    -1，表示失败
- */
 void minheap_print(minHeap *mh)
 {
-	long int i;
+	int i;
 	for (i=0; i<mh->cnt; i++)
-		printf("%ld ", mh->heap[i]);
+		printf("%d ", mh->heap[i].key);
 }
 
 
-minHeap * minheap_init(long int size)
+minHeap * minheap_init(int size)
 {
-	minHeap * mh = (minHeap *)malloc(sizeof(minHeap) + sizeof(HEAP_DTYPE) * size);
+	minHeap * mh = (minHeap *)malloc(sizeof(minHeap) + sizeof(struct Entry) * size);
 	mh->cnt = 0;
 	mh->size = size;
 	return mh;
@@ -209,34 +153,30 @@ int main(void)
 {
 	minHeap *mh = minheap_init(1024);
 
-	HEAP_DTYPE a[] = {80, 40, 30, 60, 90, 70, 10, 50, 20};
-	long int i, len=LENGTH(a);
+	int a[] = {80, 40, 30, 60, 90, 70, 10, 50, 20};
 
-	printf("== minHeapPush: ");
-	for(i=0; i<len; i++) {
-		printf("%ld ", a[i]);
-		minHeapPush(mh, a[i]);
+	int i;
+	for(i=0; i< sizeof(a) / sizeof(a[0]); i++) {
+		printf("%d ", a[i]);
+		struct Entry node;
+		node.key = a[i];
+		minheap_push(mh, node);
 	}
 
 	printf("\n== minheap_print: \n10 20 30 50 90 70 40 80 60 :\n");
 	minheap_print(mh);
 
-	i=15;
-	minHeapPush(mh, i);
-	printf("\n== minHeapPush: %ld", i);
-	printf("\n== minheap_print: \n10 15 30 50 20 70 40 80 60 90:\n");
-	minheap_print(mh);
-
 	i=10;
-	minheap_remove(H, i);
-	printf("\n== minheap_remove: %ld", i);
+	minheap_remove(mh, i);
+	printf("\n== minheap_remove: %d", i);
 	printf("\n== minheap_print: \n15 20 30 50 90 70 40 80 60 :\n");
 	minheap_print(mh);
 	printf("\n");
 
-	printf("\n minHeapPop test:\n");
+	printf("\n minheap_pop test:\n");
 	while(!minHeapisEmpty(mh)) {
-		printf(" %ld ", minHeapPop(mh));
+		struct Entry get = minheap_pop(mh);
+		printf(" %d ", get.key);
 	}
 
 	printf("\n");
