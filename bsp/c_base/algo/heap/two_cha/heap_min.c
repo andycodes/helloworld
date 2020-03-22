14bd858aa417e96377858026565466c57f5795d6
andycodes <brucefan@foxmail.com>
2020-03-22 10:57:54 +0800

    heap

--- bsp/c_base/algo/heap/two_cha/heap_min.c
+++ bsp/c_base/algo/heap/two_cha/heap_min.c
@@ -6,152 +6,169 @@ struct Entry {
 	int key;
 };
 
-typedef struct minHeap{
+struct PriorityQueue{
 	int cnt;
 	int size;
 	struct Entry heap[0];
-}minHeap;
+};
 
 
-int minHeapGetIndex(minHeap *mh, int key)
+int minHeapGetIndex(struct PriorityQueue *pq, int key)
 {
 	int i=0;
 
-	for(i = 0; i < mh->cnt; i++)
-		if (key == mh->heap[i].key)
+	for(i = 0; i < pq->cnt; i++)
+		if (key == pq->heap[i].key)
 			return i;
 
 	return -1;
 }
 
 
-bool minHeapisEmpty(minHeap *mh)
+bool PriorityQueue_isEmpty(struct PriorityQueue *pq)
 {
-	return mh->cnt == 0;
+	return pq->cnt == 0;
 }
 
-bool minHeapisFull(minHeap *mh)
+bool PriorityQueue_isFull(struct PriorityQueue *pq)
 {
-	printf("heap is full\n");
-	return mh->cnt == mh->size;
-}
-
-
-int minHeapGetSize(minHeap *mh)
-{
-	return mh->cnt;
+	return pq->cnt == pq->size;
 }
 
 
-static void minheap_filterdown(minHeap *mh, int start, int end)
+int minHeapGetSize(struct PriorityQueue *pq)
+{
+	return pq->cnt;
+}
+
+
+static void minheap_filterdown(struct PriorityQueue *pq, int start, int end)
 {
 	int curNode = start;
 	int leftChild = 2*curNode + 1;
-	struct Entry tmp = mh->heap[curNode];
+	struct Entry tmp = pq->heap[curNode];
 
 	while(leftChild <= end) {
-		if(leftChild < end && mh->heap[leftChild].key > mh->heap[leftChild+1].key)
+		if(leftChild < end && pq->heap[leftChild].key > pq->heap[leftChild+1].key)
 			leftChild++;
 
-		if(tmp.key <= mh->heap[leftChild].key)
+		if(tmp.key <= pq->heap[leftChild].key)
 			break;
 		else {
-			mh->heap[curNode] = mh->heap[leftChild];
+			pq->heap[curNode] = pq->heap[leftChild];
 			curNode = leftChild;
 			leftChild = 2*leftChild + 1;
 		}
 	}
 
-	mh->heap[curNode] = tmp;
+	pq->heap[curNode] = tmp;
 }
 
 
-int minheap_remove(minHeap *mh, int data)
+int minheap_remove(struct PriorityQueue *pq, int data)
 {
 	int index;
-	if(mh->cnt == 0)
+	if(pq->cnt == 0)
 		return  -1;
 
-	index = minHeapGetIndex(mh, data);
+	index = minHeapGetIndex(pq, data);
 	if (index== -1)
 		return -1;
 
-	mh->heap[index] = mh->heap[--mh->cnt];
-	minheap_filterdown(mh, index, mh->cnt-1);
+	pq->heap[index] = pq->heap[--pq->cnt];
+	minheap_filterdown(pq, index, pq->cnt-1);
 
 	return 0;
 }
 
 
-struct Entry  minheap_pop(minHeap *mh)
+struct Entry  minheap_pop(struct PriorityQueue *pq)
 {
 	struct Entry min;
 
-	min = mh->heap[0];
-	minheap_remove(mh, min.key);
+	min = pq->heap[0];
+	minheap_remove(pq, min.key);
 	return min;
 }
 
 
-static void filter_up(minHeap *mh, int start)
+static void filter_up(struct PriorityQueue *pq, int start)
 {
 	int curNode = start;
 	int parent = (curNode - 1)/2;
-	struct Entry tmp = mh->heap[curNode];
+	struct Entry tmp = pq->heap[curNode];
 
 	while(curNode > 0) {
-		if(mh->heap[parent].key <= tmp.key)
+		if(pq->heap[parent].key <= tmp.key)
 			break;
 		else {
-			mh->heap[curNode] = mh->heap[parent];
+			pq->heap[curNode] = pq->heap[parent];
 			curNode = parent;
 			parent = (parent-1)/2;
 		}
 	}
 
-	mh->heap[curNode] = tmp;
+	pq->heap[curNode] = tmp;
 }
 
-int minheap_push(minHeap *mh, struct Entry node)
+#if 0
+int priorityQueue_push(struct PriorityQueue *pq, struct Entry node)
 {
-	if(minHeapisFull(mh)) {
+	if(PriorityQueue_isFull(pq)) {
 		return -1;
 	}
 
-	mh->heap[mh->cnt] = node;
-	filter_up(mh, mh->cnt);
-	mh->cnt++;
+	pq->heap[pq->cnt] = node;
+	filter_up(pq, pq->cnt);
+	pq->cnt++;
 
 	return 0;
 }
-
-
-void minheap_print(minHeap *mh)
+#else
+void priorityQueue_push( struct PriorityQueue * pq, struct Entry node)
 {
 	int i;
-	for (i=0; i<mh->cnt; i++)
-		printf("%d ", mh->heap[i].key);
+
+	if ( PriorityQueue_isFull( pq ) )
+	{
+		printf( "Priority queue is full" );
+		return ;
+	}
+
+	for ( i = ++pq->cnt; pq->heap[i / 2].key > node.key; i /= 2)
+		pq->heap[ i ] = pq->heap[i / 2];
+
+	pq->heap[i] = node;
+}
+#endif
+
+
+void minheap_print(struct PriorityQueue *pq)
+{
+	int i;
+	for (i=0; i<pq->cnt; i++)
+		printf("%d ", pq->heap[i].key);
 }
 
 
-minHeap * minheap_init(int size)
+struct PriorityQueue * minheap_init(int size)
 {
-	minHeap * mh = (minHeap *)malloc(sizeof(minHeap) + sizeof(struct Entry) * size);
-	mh->cnt = 0;
-	mh->size = size;
-	return mh;
+	struct PriorityQueue * pq = (struct PriorityQueue *)malloc(sizeof(struct PriorityQueue) + sizeof(struct Entry) * size);
+	pq->cnt = 0;
+	pq->size = size;
+	return pq;
 }
 
 
-void minheap_exit(minHeap * mh)
+void minheap_exit(struct PriorityQueue * pq)
 {
-	free(mh);
-	mh = NULL;
+	free(pq);
+	pq = NULL;
 }
 
 int main(void)
 {
-	minHeap *mh = minheap_init(1024);
+	struct PriorityQueue *pq = minheap_init(1024);
 
 	int a[] = {80, 40, 30, 60, 90, 70, 10, 50, 20};
 
@@ -160,22 +177,22 @@ int main(void)
 		printf("%d ", a[i]);
 		struct Entry node;
 		node.key = a[i];
-		minheap_push(mh, node);
+		priorityQueue_push(pq, node);
 	}
 
 	printf("\n== minheap_print: \n10 20 30 50 90 70 40 80 60 :\n");
-	minheap_print(mh);
+	minheap_print(pq);
 
 	i=10;
-	minheap_remove(mh, i);
+	minheap_remove(pq, i);
 	printf("\n== minheap_remove: %d", i);
 	printf("\n== minheap_print: \n15 20 30 50 90 70 40 80 60 :\n");
-	minheap_print(mh);
+	minheap_print(pq);
 	printf("\n");
 
 	printf("\n minheap_pop test:\n");
-	while(!minHeapisEmpty(mh)) {
-		struct Entry get = minheap_pop(mh);
+	while(!PriorityQueue_isEmpty(pq)) {
+		struct Entry get = minheap_pop(pq);
 		printf(" %d ", get.key);
 	}
 
