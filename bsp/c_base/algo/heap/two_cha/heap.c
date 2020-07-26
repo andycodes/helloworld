919d94b9978018cf80fa92420219468016f209ed
andycodes <brucefan@foxmil.com>
2020-07-26 17:05:08 +0800

    felix

--- bsp/c_base/algo/heap/two_cha/heap.c
+++ bsp/c_base/algo/heap/two_cha/heap.c
@@ -6,111 +6,112 @@
 (03) 索引为i的父结点的索引是 floor((i-1)/2);
 
 二叉堆的核心是"添加节点"和"删除节点"
+
+第K小元素 ----大堆栈
 */
 
 struct heapEntry {
 	int key;
-	int value;
 };
 
 struct HeapCtrl{
 	int size;
-	int capacity;
-	void (*remove_down)(struct HeapCtrl *, int , int);
-	void (*push_up)(struct HeapCtrl *, int);
-	struct heapEntry heap[0];
+	int cap;
+	void (*rmdown)(struct HeapCtrl *, int , int);
+	void (*pushup)(struct HeapCtrl *, int);
+	struct heapEntry node[0];
 };
 
-#ifdef PRIORITY_QUEUE_H
-void maxheap_pushup(struct HeapCtrl *hp, int start)
+#ifdef PRIORITY_QUEUE_MAX
+void maxheapPushup(struct HeapCtrl *hp, int start)
 {
 	int child = start; // 当前节点(current)的位置
 	int p = (child - 1) / 2;// 父(parent)结点的位置
-	struct heapEntry cur = hp->heap[child];// 当前节点(current)的大小
+	struct heapEntry cur = hp->node[child];// 当前节点(current)的大小
 
 	while(child > 0) {
-		if(hp->heap[p].key >= cur.key) {
+		if(hp->node[p].key >= cur.key) {
 			break;
 		} else {
-			hp->heap[child] = hp->heap[p];
+			hp->node[child] = hp->node[p];
 			child = p;
 			p = (child - 1) / 2;
 		}
 	}
 
-	hp->heap[child] = cur;
+	hp->node[child] = cur;
 }
 
-void maxheap_rmdown(struct HeapCtrl *hp, int start, int end)
+void maxheapRmdown(struct HeapCtrl *hp, int start, int end)
 {
 	int p = start;
 	int lchild = 2*p + 1;
-	struct heapEntry cur = hp->heap[p];
+	struct heapEntry cur = hp->node[p];
 
 	while(lchild <= end) {
-		if(lchild < end && hp->heap[lchild].key < hp->heap[lchild+1].key)
+		if(lchild < end && hp->node[lchild].key < hp->node[lchild+1].key)
 			lchild++;// 左右两孩子中选择较大者，即m_heap[l+1]
 
-		if(cur.key >= hp->heap[lchild].key) {
+		if(cur.key >= hp->node[lchild].key) {
 			break;//调整结束
 		} else {
-			hp->heap[p] = hp->heap[lchild];
+			hp->node[p] = hp->node[lchild];
 			p = lchild;
 			lchild = 2*p + 1;
 		}
 	}
 
-	hp->heap[p] = cur;
+	hp->node[p] = cur;
 }
 #endif
 
 #ifdef PRIORITY_QUEUE_MIN
-void minheap_pushup(struct HeapCtrl *hp, int start)
+void minheapPushup(struct HeapCtrl *hp, int start)
 {
-	int cur = start;
-	int p = (cur - 1) / 2;
-	struct heapEntry newNode = hp->heap[cur];
+	int child = start;
+	int p = (child - 1) / 2;
+	struct heapEntry cur = hp->node[child];
 
-	while(cur > 0) {
-		if (hp->heap[p].key <= newNode.key) {
+	while(child > 0) {
+		if (hp->node[p].key <= cur.key) {
 			break;
 		} else {
-			hp->heap[cur] = hp->heap[p];
-			cur = p;
-			p = (cur - 1) / 2;
+			hp->node[child] = hp->node[p];
+			child = p;
+			p = (child - 1) / 2;
 		}
 	}
 
-	hp->heap[cur] = newNode;
+	hp->node[child] = cur;
 }
 
-void minheap_rmdown(struct HeapCtrl *hp, int start, int end)
+void minheapRmdown(struct HeapCtrl *hp, int start, int end)
 {
 	int p = start;
 	int lchild = 2*p + 1;
-	struct heapEntry cur = hp->heap[p];
+	struct heapEntry cur = hp->node[p];
 
 	while(lchild <= end) {
-		if(lchild < end && hp->heap[lchild].key > hp->heap[lchild+1].key)
+		if(lchild < end && hp->node[lchild].key > hp->node[lchild+1].key)
 			lchild++;// 左右两孩子中选择较小者，即m_heap[l+1]
 
-		if(cur.key <= hp->heap[lchild].key)
+		if(cur.key <= hp->node[lchild].key)
 			break;//调整结束
 		else {
-			hp->heap[p] = hp->heap[lchild];
+			hp->node[p] = hp->node[lchild];
 			p = lchild;
 			lchild = 2*lchild + 1;
 		}
 	}
 
-	hp->heap[p] = cur;
+	hp->node[p] = cur;
 }
 #endif
 
-int heap_getIdx(struct HeapCtrl *hp, int key)
+int heapGetIdx(struct HeapCtrl *hp, int key)
 {
 	for(int i = 0; i < hp->size; i++) {
-		if (key == hp->heap[i].key) {
+		if (key == hp->node[i].key) {
 			return i;
 		}
 	}
@@ -118,66 +119,66 @@ int heap_getIdx(struct HeapCtrl *hp, int key)
 	return -1;
 }
 
-bool heap_empty(struct HeapCtrl *hp)
+bool heapEmpty(struct HeapCtrl *hp)
 {
 	return hp->size == 0;
 }
 
-bool heap_full(struct HeapCtrl *hp)
+bool heapFull(struct HeapCtrl *hp)
 {
-	return hp->size == hp->capacity;
+	return hp->size == hp->cap;
 }
 
-int heap_size(struct HeapCtrl *hp)
+int heapSize(struct HeapCtrl *hp)
 {
 	return hp->size;
 }
 
-int heap_remove(struct HeapCtrl *hp, int data)
+int heapRm(struct HeapCtrl *hp, int data)
 {
 	int index;
 	if(hp->size == 0)
 		return  -1;
 
-	index = heap_getIdx(hp, data);
+	index = heapGetIdx(hp, data);
 	if (index== -1) {
 		return -1;
 	}
 
-	hp->heap[index] = hp->heap[--hp->size];// 用最后元素填补
+	hp->node[index] = hp->node[--hp->size];// 用最后元素填补
 	// 从index位置开始自上向下调整为最大堆
-	hp->remove_down(hp, index, hp->size -1);
+	hp->rmdown(hp, index, hp->size -1);
 	return 0;
 }
 
-struct heapEntry heap_pop(struct HeapCtrl *hp)
+struct heapEntry heapPop(struct HeapCtrl *hp)
 {
 	struct heapEntry top;
 
-	top = hp->heap[0];
-	heap_remove(hp, top.key);
+	top = hp->node[0];
+	heapRm(hp, top.key);
 	return top;
 }
 
-int heap_push(struct HeapCtrl *hp, struct heapEntry node)
+int heapPush(struct HeapCtrl *hp, struct heapEntry node)
 {
-	if(heap_full(hp)) {
+	if(heapFull(hp)) {
 		return -1;
 	}
 
-	hp->heap[hp->size] = node;
-	hp->push_up(hp, hp->size);
+	hp->node[hp->size] = node;
+	hp->pushup(hp, hp->size);
 	hp->size++;
 
 	return 0;
 }
 
-struct HeapCtrl *heap_init(int capacity)
+struct HeapCtrl *heapInit(int cap)
 {
-	struct HeapCtrl * hp = (struct HeapCtrl *)calloc(1, sizeof(struct HeapCtrl) + sizeof(struct heapEntry) * capacity);
-	hp->capacity = capacity;
-	hp->remove_down = maxheap_rmdown;//minheap_rmdown
-	hp->push_up = maxheap_pushup;
+	struct HeapCtrl * hp = (struct HeapCtrl *)calloc(1, sizeof(struct HeapCtrl) + sizeof(struct heapEntry) * cap);
+	hp->cap = cap;
+	hp->rmdown = maxheapRmdown;//minheapRmdown
+	hp->pushup = maxheapPushup;
 	return hp;
 }
 
--- bsp/c_base/algo/heap/two_cha/heap_test.c
+++ bsp/c_base/algo/heap/two_cha/heap_test.c
@@ -1,4 +1,58 @@
 /*
+373. 查找和最小的K对数字
+给定两个以升序排列的整形数组 nums1 和 nums2, 以及一个整数 k。
+定义一对值 (u,v)，其中第一个元素来自 nums1，
+第二个元素来自 nums2。
+找到和最小的 k 对数字 (u1,v1), (u2,v2) ... (uk,vk)。
+示例 1:
+
+输入: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+输出: [1,2],[1,4],[1,6]
+解释: 返回序列中的前 3 对数：
+     [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+示例 2:
+*/
+int** kSmallestPairs(int* nums1, int nums1Size,
+int* nums2, int nums2Size, int k,
+int* returnSize, int** returnColumnSizes)
+{
+	struct HeapCtrl *hp = heapInit(k);
+
+	for (int i = 0; i < nums1Size; i++) {
+		for (int j = 0; j < nums2Size; j++) {
+			if (heapFull(hp)) {
+				int sum = nums1[i] + nums2[j];
+				struct heapEntry top = hp->node[0];
+				if (sum >= top.key) {
+					continue;
+				} else {
+					heapPop(hp);
+				}
+			}
+
+			struct heapEntry node;
+			node.n1 = nums1[i];
+			node.n2 = nums2[j];
+			node.key = node.n1 + node.n2;
+			heapPush(hp, node);
+		}
+	}
+
+	int size = hp->size;
+	*returnSize = size;
+	int **res = (int **)calloc(size, sizeof(int *));
+	*returnColumnSizes = (int *)calloc(size, sizeof(int));
+	for (int i = 0; i < size; i++) {
+		res[i] = (int *)calloc(2, sizeof(int));
+		res[i][0] = hp->node[i].n1;
+		res[i][1] = hp->node[i].n2;
+		(*returnColumnSizes)[i] = 2;
+	}
+
+	return res;
+}
+
+/*
 264. 丑数 II
 编写一个程序，找出第 n 个丑数。
 
@@ -47,121 +101,38 @@ int nthUglyNumber(int n)
 
 
 /*
-题目描述
-评论 (62)
-题解(42)
-提交记录
-373. 查找和最小的K对数字
-给定两个以升序排列的整形数组 nums1 和 nums2, 以及一个整数 k。
-
-定义一对值 (u,v)，其中第一个元素来自 nums1，第二个元素来自 nums2。
-
-找到和最小的 k 对数字 (u1,v1), (u2,v2) ... (uk,vk)。
-
-示例 1:
-
-输入: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
-输出: [1,2],[1,4],[1,6]
-解释: 返回序列中的前 3 对数：
-     [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
-示例 2:
-
-输入: nums1 = [1,1,2], nums2 = [1,2,3], k = 2
-输出: [1,1],[1,1]
-解释: 返回序列中的前 2 对数：
-     [1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
-示例 3:
-
-输入: nums1 = [1,2], nums2 = [3], k = 3
-输出: [1,3],[2,3]
-解释: 也可能序列中所有的数对都被返回:[1,3],[2,3]
-
-*/
-/**
- * Return an array of arrays of size *returnSize.
- * The sizes of the arrays are returned as *returnColumnSizes array.
- * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
- */
-int** kSmallestPairs(int* nums1, int nums1Size, int* nums2,
-int nums2Size, int k, int* returnSize, int** returnColumnSizes)
-{
-	if (nums1 == NULL || nums2 == NULL || nums1Size <= 0 || nums2Size <= 0 || k <= 0) {
-		*returnSize = 0;
-		return NULL;
-	}
-
-	struct PriorityQueue *pq = priorityQueue_init(k, PRIORITY_QUEUE_MAX);
-
-	for (int i = 0; i < nums1Size; i++) {
-		for (int j = 0; j < nums2Size; j++) {
-			if (priorityQueue_isFull(pq)) {
-				int sum = nums1[i] + nums2[j];
-				struct Entry top = priorityQueue_top(pq);
-				if (sum < top.key) {
-					priorityQueue_pop(pq);
-					struct Entry newNode;
-					newNode.key = sum;
-					newNode.num1 = nums1[i];
-					newNode.num2 = nums2[j];
-					priorityQueue_push(pq, newNode);
-				}
-			} else {
-					struct Entry newNode;
-					newNode.key = nums1[i] + nums2[j];
-					newNode.num1 = nums1[i];
-					newNode.num2 = nums2[j];
-					priorityQueue_push(pq, newNode);
-			}
-		}
-	}
-
-	int ** res = (int **)calloc(1024 * 1024, sizeof(int));
-	*returnColumnSizes = (int *)calloc(1024, sizeof(int));
-	*returnSize = priorityQueue_size(pq);
-	int i = 0;
-	while(!priorityQueue_isEmpty(pq)) {
-		res[i] = (int *)calloc(1024, sizeof(int));
-		struct Entry newNode = priorityQueue_pop(pq);
-		res[i][0] = newNode.num1;
-		res[i][1] = newNode.num2;
-		(*returnColumnSizes)[i] = 2;
-		i++;
-	}
-
-	return res;
-}
-
-
-/*
 面试题 17.09. 第 k 个数
 难度中等13
-有些数的素因子只有 3，5，7，请设计一个算法找出第 k 个数。注意，不是必须有这些素因子，而是必须不包含其他的素因子。例如，前几个数按顺序应该是 1，3，5，7，9，15，21。
+有些数的素因子只有 3，5，7，请设计一个算法找出第 k 个数。
+注意，不是必须有这些素因子，而是必须不包含其他的素因子。
+例如，前几个数按顺序应该是 1，3，5，7，9，15，21。
 示例 1:
 输入: k = 5
 
 输出: 9
 
 */
-
 int getKthMagicNumber(int k){
 	int rat[] = {3,5,7};
 	int ret = 0;
-	struct PriorityQueue *pq =  priorityQueue_init(1024,  PRIORITY_QUEUE_MIN);
-	struct DataEntry node;
+	struct HeapCtrl *pq =  heapInit(1024);
+	struct heapEntry node;
 	node.key = 1;
-	priorityQueue_push(pq, node);
-	while(k-- && !priorityQueue_isEmpty(pq)) {
-		struct DataEntry top =  priorityQueue_pop(pq);
+	heapPush(pq, node);
+	while(k-- && !heapEmpty(pq)) {
+		struct heapEntry top =  heapPop(pq);
 		ret = top.key;
 		for(int i = 0; i < 3 ;i++) {
-			struct DataEntry node;
+			struct heapEntry node;
 			node.key = top.key * rat[i];
-			priorityQueue_push_sole(pq, node);
+			heapPush(pq, node);
 		}
 	}
 
 	return ret;
 }
+
+
 /*
 378. 有序矩阵中第K小的元素
 bseach
