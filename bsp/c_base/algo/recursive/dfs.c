63dbde708a0efa593a6846a0b09c5039c170ccd7
andycodes <brucefan@foxmil.com>
2020-08-08 08:42:48 +0800

    string

--- bsp/c_base/algo/recursive/dfs.c
+++ bsp/c_base/algo/recursive/dfs.c
@@ -1509,3 +1509,66 @@ char ** removeInvalidParentheses(char * s, int* returnSize){
     return returnArry;
 }
 
+/*
+93. 复原IP地址
+难度中等320
+给定一个只包含数字的字符串，复原它并返回所有可能的 IP 地址格式。
+有效的 IP 地址正好由四个整数（每个整数位于 0 到 255 之间组成），整数之间用 '.' 分隔。
+
+示例:
+输入: "25525511135"
+输出: ["255.255.11.135", "255.255.111.35"]
+*/
+#define MAX_SIZE 1024
+
+bool isValid(char *s, int len) {
+    if (s == '\0') {
+        return false;
+    }
+
+    if (s[0] == '0' && len > 1) {
+        return false;
+    }
+    int num = 0;
+    for (int i = 0; i < len; i++) {
+        num = num * 10 + s[i] - '0';
+    }
+    if (num >= 0 && num <= 255) {
+        return true;
+    }
+    return false;
+}
+
+void dfs(char *s, char **res, int *returnSize, char *str, int idx, int validNum)
+{
+    if (validNum > 4) {
+        return;
+    }
+    if (s[0] == '\0' && validNum == 4) { // 匹配完了
+        res[*returnSize] = (char *)malloc(strlen(str) + 1);
+        strcpy(res[*returnSize], str);
+        (*returnSize)++;
+    }
+
+    if (validNum > 0 && validNum < 4) {
+        str[idx++] = '.';
+    }
+    for (int len = 1; len <= 3 && len <= strlen(s); len++) {
+        if (isValid(s, len)) {
+
+            for (int i = 0; i < len; i++) {
+                str[idx + i] = s[i];
+            }
+            dfs(s + len, res, returnSize, str, idx + len, validNum + 1);
+        }
+    }
+
+}
+char ** restoreIpAddresses(char * s, int* returnSize){
+    char **res = (char **)malloc(MAX_SIZE * sizeof(char *));
+    *returnSize = 0;
+    char *str = (char *)calloc(strlen(s) + 4, sizeof(char));
+    dfs(s, res, returnSize, str, 0, 0);
+    return res;
+
+}
--- bsp/c_base/algo/string/string.c
+++ bsp/c_base/algo/string/string.c
@@ -1,24 +1,29 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+/*
+(1) 如果格式化后的字符串长度 < size，
+则将此字符串全部复制到str中，
+并给其后添加一个字符串结束符('\0')；
+(2) 如果格式化后的字符串长度 >= size，
+则只将其中的(size-1)个字符复制到str中，
+并给其后添加一个字符串结束符('\0')，
+返回值为欲写入的字符串长度。
+返回值为字符串的真实长度。
 
-#include "../../flag_type.h"
-#include "../math/math.h"
+一定有'\0';最大拷贝size -1长度用户数据
+*/
+int snprintf ( char * str, size_t size, const char * format, ... );
+
 
 void digit2Str_sprintf(char *str, int digit)
 {
 	sprintf(str, "%d", digit);
 	sprintf(str, "%x", digit);
 }
+
 /*
 isdigit
-
 函数名称: isdigit
-
 函数原型: int isdigit(char ch);
-
 函数功能: 检查ch是否是数字(0-9)
-
 函数返回: 是返回非0,否则返回0
 */
 int isdigit(char ch)
@@ -30,32 +35,40 @@ int isdigit(char ch)
 }
 
 /*字符串转整形*/
-int atoi(const char *str)
-{
-	int num = 0;
+int atoi(char * str){
+	long int num = 0;
 	int sign = 1;
 	const int len = strlen(str);
 	int i = 0;
 
-	while (str[i] == ' ' && i < len)i++;
+	while (str[i] == ' ' && i < len)
+		i++;
 
-	if (str[i] == '+') i++;
-	if (str[i] == '-') {
+	if (str[i] == '+')
+		i++;
+	else if (str[i] == '-') {
 		sign = -1;
 		i++;
 	}
 
 	for (; i < len; i++) {
-		if (str[i] < '0' || str[i] > '9')
+		if (str[i] < '0' || str[i] > '9')//if (!isdigit(str[i]))
 			break;
-		if (num > INT_MAX / 10 ||  (num == INT_MAX / 10 && (str[i] - '0') > INT_MAX % 10)) {
+		//if (num > INT_MAX / 10 ||  (num == INT_MAX / 10 && (str[i] - '0') > INT_MAX % 10)) {
+		//	return sign == -1 ? INT_MIN : INT_MAX;
+		//}
+
+		num = num * 10 + str[i] - '0';
+		if ((int)num != num) {
 			return sign == -1 ? INT_MIN : INT_MAX;
 		}
-
-		num = str[i] - '0' + num * 10;
 	}
-	return num * sign;
+
+	return (int)(num * sign);
+
 }
+long int atol(char * str);//将字符串转换为长整型值。
+float atof(char * str);//将字符串转换为双精度浮点型值。
 
 void str2digit_sscanf(char * str)
 {
@@ -63,29 +76,6 @@ void str2digit_sscanf(char * str)
 	sscanf(str, "%x", &value);
 	printf("0x%x\n",value);
 }
-
-void str2digit_strtol(char * str)
-{
-	char* p;
-	long value = strtol(str, &p, 16);
-	printf("0x%lx\n",value);
-}
-
-unsigned long str2digit(char *str, int len)
-{
-	unsigned long i, data = 0;
-
-	for(i = 0; i < len; i++) {
-		if (!isdigit(str[i]))
-			return -1;
-		data *= 10;
-		data += str[i]-'0';
-	}
-
-	return data;
-}
-
-
 
 /*
 #include <string.h>
@@ -111,70 +101,26 @@ strchr() 函数会依次检索字符串 str 中的每一个字符，
 */
 char* strchr(const char* str, int c);
 
-/*
-2. string/array to int/float
-C/C++语言提供了几个标准库函数，可以将字符串转换为任意类型(整型、长整型、浮点型等)。
-● atof()：将字符串转换为双精度浮点型值。
-● atoi()：将字符串转换为整型值。
-● atol()：将字符串转换为长整型值。
-● strtod()：将字符串转换为双精度浮点型值，并报告不能被转换的所有剩余数字。
-● strtol()：将字符串转换为长整值，并报告不能被转换的所有剩余数字。
-● strtoul()：将字符串转换为无符号长整型值，并报告不能被转换的所有剩余数字。
-*/
-
-/*
-maxlen：表示复制的字符串长度
-（c/c++）复制字符串source中的内容（字符，数字、汉字....）
-到字符串destinin中，复制多少由maxlen的值决定。
-如果source的前n个字符不含NULL字符，
-则结果不会以NULL字符结束。
-如果n<source的长度，只是将source的前n个字符复制到
-destinin的前n个字符，不自动添加'\0'，
-也就是结果destinin不包括'\0'，需要再手动添加一个'\0'。
-如果source的长度小于n个字节，则以NULL填充
-destinin直到复制完n个字节。
-source和destinin所指内存区域不可以重叠且
-destinin必须有足够的空间来容纳source的字符长度+'\0'。
-*/
-/*
-char *strncpy(char *destinin, char *source, int maxlen);
-destinin[maxlen] = '\0';
-*/
-
-/*
-提取字符串
-从src start 位置提取长度为len的字符串到dst
-*/
-void substr(char* dst, char* src, int start, int cpLen)
+void strreplace(char *text, char *oldsubstr, char *newsubstr)
 {
-#if 0
-    char* sc = src + start;
-    int srcLen = strlen(sc);
-    int i = 0;
+	int oldsublen = strlen(oldsubstr);
+	int newsublen = strlen(newsubstr);
+	char *temp = text;
 
-
-    if (srcLen < cpLen) {
-        cpLen = srcLen;
-    }
-
-    while (srcLen) {
-        dst[i] = sc[i];
-        srcLen--;
-        i++;
-    }
-    dst[i] = '\0';
-#else
-	strncpy(dst, src + start, cpLen);
-	dst[cpLen] = '\0';
-#endif
+	while ( (temp = strstr(temp, oldsubstr)) != NULL ) {
+		memmove(temp + newsublen, temp + oldsublen, strlen(temp) - oldsublen + 1);
+		memcpy(temp, newsubstr, newsublen);
+		temp = temp + newsublen;
+	}
 }
 
-char *strrpl(char *srcStr, char *dstStr, int dstSize, const char *oldSubStr, char *newSubStr)
+char *strrpl(char *dstStr, int dstSize, char *srcStr, const char *oldSubStr, char *newSubStr)
 {
 	char *it = srcStr;
 	unsigned int  len = dstSize - 1;
 
-	if(oldSubStr == NULL ||newSubStr== NULL || srcStr == NULL || dstStr  == NULL || dstSize <= 0) {
+	if(oldSubStr == NULL ||newSubStr == NULL || srcStr == NULL
+		|| dstStr  == NULL || dstSize <= 0) {
 		return NULL;
 	}
 
@@ -199,43 +145,17 @@ char *strrpl(char *srcStr, char *dstStr, int dstSize, const char *oldSubStr, cha
 	return dstStr;
 }
 
-void strreplace(char *text, char *oldsubstr, char *newsubstr)
-{
-	int oldsublen = strlen(oldsubstr);
-	int newsublen = strlen(newsubstr);
-	char *temp = text;
-
-	while ( (temp = strstr(temp, oldsubstr)) != NULL ) {
-		memmove(temp + newsublen, temp + oldsublen, strlen(temp) - oldsublen + 1);
-		memcpy(temp, newsubstr, newsublen);
-		temp = temp + newsublen;
-	}
-}
-
 
 /*
-子序列
+392. 判断子序列
 给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
-
 你可以认为 s 和 t 中仅包含英文小写字母。
-字符串 t 可能会很长（长度 ~= 500,000），
-而 s 是个短字符串（长度 <=100）。
-
-字符串的一个子序列是原始字符串删除
-一些（也可以不删除）字符而不改变剩
-余字符相对位置形成的新字符串。（例如，"ace"是"abcde"的一
-个子序列，而"aec"不是）。
-
 示例 1:
 s = "abc", t = "ahbgdc"
-
 返回 true.
-
 示例 2:
 s = "axc", t = "ahbgdc"
-
 返回 false.
-
 */
 bool isSubsequence(char * s, char * t)
 {
@@ -244,18 +164,20 @@ bool isSubsequence(char * s, char * t)
 	unsigned int s_size = strlen(s);
 	unsigned int t_size = strlen(t);
 
-    if(s_size == 0)
-        return true;
+	if(s_size == 0)
+		return true;
 
 	for(i = 0,j = 0; i< t_size;i++) {
 		if(t[i] == s[j]){
 			j++;
 		}
-        if(j == s_size) return true;
+
+		if(j == s_size) return true;
 	}
 
 	return false;
 }
+
 
 /*删除首尾空格*/
 char* trim(char *a)
@@ -276,30 +198,11 @@ p1会移到p2的后面，那么如果p1>p2，返回空。*/
 	p1[len] = '\0';
 	return p1;
 }
-
-void deblank(char string[])
+/*删除多余空格*/
+char * deblank(char * str)
 {
-    int i=0;
-    int j=0;
-    while(string[j]!='\0'){
-        if(string[j]!=' '||string[j+1]!=' '){
-            string[i]=string[j];
-            i++;
-            j++;
-        }
-        else
-        {
-            string[i]=string[j+1];
-            j++;
-        }
-    }
-    string[i]='\0';
-}
-
-char * deblank_trim(char * str)
-{
-    char * left = str;//前面的指针，负责赋值
-    char * right = str;//后面寻找非空格字符的指针
+    char *left = str;//前面的指针，负责赋值
+    char *right = str;//后面寻找非空格字符的指针
 
     while (*right != '\0') {
         if (*right != ' ') {
@@ -309,8 +212,8 @@ char * deblank_trim(char * str)
             left++;
         }
 
-        if (*right == ' ' && *(right + 1) != ' ')//寻找到下一个单词时，对left的操作
-        {
+//寻找到下一个单词时，对left的操作
+        if (*right == ' ' && *(right + 1) != ' ') {
             *left = ' ';
             left++;
         }
@@ -346,17 +249,13 @@ void strrev(char* s)
 
 /*
 给定一个字符串，逐个翻转字符串中的每个单词。
-
 示例：
-
 输入: ["t","h","e"," ","s","k","y"," ","i","s"," ","b","l","u","e"]
 输出: ["b","l","u","e"," ","i","s"," ","s","k","y"," ","t","h","e"]
 注意：
-
 单词的定义是不包含空格的一系列字符
 输入字符串中不会包含前置或尾随的空格
 单词与单词之间永远是以单个空格隔开的
-
 */
 void reverseWords(char* s, int sSize)
 {
@@ -374,23 +273,38 @@ void reverseWords(char* s, int sSize)
 }
 
 
-size_t strlen(const char *str) {
+size_t strlen(const char *str)
+{
 	const char *s;
 	for (s = str; *s; ++s) {}
 	return(s - str);
 }
 
 
-char* strcpy(char *to, const char *from) {
-    if(to == NULL && from == NULL)
-		return 0;
+char* strcpy(char *dest, const char *src)
+{
+    if(dest == NULL || src == NULL)
+		return NULL;
 
-    char *p = to;
-    while ((*p++ = *from++) != '\0')
-    ;
-    return to;
+    char *p = dest;
+    while ((*p++ = *src++) != '\0');
+    return dest;
+}
+/*当src len 大于maxlen 则截断复制maxlen到dest,
+dest尾部没有结束符'\0
+dest[maxlen] = '\0''*/
+char *strncpy(char *dest, char *src, int maxlen);
+/*
+提取字符串
+从src start 位置提取长度为len的字符串到dst
+*/
+void substr(char* dst, char* src, int start, int cpLen)
+{
+	strncpy(dst, src + start, cpLen);
+	dst[cpLen] = '\0';
 }
 
+char * strncat(char *dest, const char *src, size_t n);
 
 /*利用或操作 | 和空格将英文字符转换为小写
 ('a' | ' ') = 'a'
@@ -423,49 +337,6 @@ char *alpha_low_upper( char *Str )
 
     return Str;
 }
-
-
-
-/*
-将一个无符号短整形数从网络字
-节顺序转换为主机字节顺序
-*/
-unsigned short ntohs(unsigned short s)
-{
-    return (s >> 8) | (s << 8);
-}
-
-/*
-将一个无符号长整形数从
-网络字节顺序转换为主机字节顺序
-
-*/
-unsigned long ntohl(unsigned long l)
-{
-    return  ((l >> 24) & 0x000000ff) |
-        ((l >>  8) & 0x0000ff00) |
-        ((l <<  8) & 0x00ff0000) |
-        ((l << 24) & 0xff000000);
-}
-
-/*
-
-将主机的无符号短整形数转换成网络字节顺序
-*/
-unsigned short htons(unsigned short s)
-{
-    return (s >> 8) | (s << 8);
-}
-
-/*
-将主机的无符号长整形数转换成网络字节顺序
-
-*/
-unsigned long htonl(unsigned long l)
-{
-    return ntohl(l);
-}
-
 
 
 /* 最长带+-符号 小数点的连续数字串*/
@@ -574,203 +445,83 @@ void strtok_test(void)
 	}
 }
 
-unsigned long strtobcd(char *s)
-{
-    unsigned long ret;
-    int i;
-
-    ret = 0;
-
-    while (*s != '\0') {
-        if (*s >= '0' && *s <= '9')
-            i = *s - '0';
-        else
-            return -1;
-        ret = (ret << 4) + i;
-        s++;
-    }
-
-    return ret;
-}
-
-#include <ctype.h>
 /*
 isalpha
-
 函数名称: isalpha
-
 函数原型:
-
 函数功能: 检查ch是否是字母.
-
 函数返回: 是字母返回非0(在vs2015中为2) ,否则返回 0.
 */
 int isalpha(char ch);
-
-
 /*
-
-iscntrl
-
-函数名称: iscntrl
-
-函数原型: int iscntrl(int ch);
-
-函数功能: 检查ch是否控制字符(其ASCII码在0和0x1F之间,数值为 0-31).
-
-函数返回: 是返回非0,否则返回 0.
-
-参数说明:
-
-*/
-
-
-
-/*
-
-isgraph
-
-函数名称: isgraph
-
-函数原型: int isgraph(int ch);
-
-函数功能: 检查ch是否可显示字符(其ASCII码在0x21到0x7E之间),不包括空格
-
-函数返回: 是返回非0,否则返回0
-
-
 islower
-
 函数名称: islower
-
 函数原型: int islower(int ch);
-
 函数功能: 检查ch是否小写字母(a-z)
-
 函数返回: 是返回非0,否则返回0
-
 参数说明:
-
 所属文件: <ctype.h>
-
-isupper
-
-函数名称: isupper
-
-函数原型: int isupper(int ch);
-
-函数功能: 检查ch是否是大写字母(A-Z)
-
-函数返回: 是返回非0,否则返回0
-
-
-tolower
-
-函数名称: tolower
-
-函数原型: int tolower(int ch);
-
-函数功能: 将ch字符转换为小写字母
-
-函数返回: 返回ch所代表的字符的小写字母
-
-
-toupper
-
-函数名称: toupper
-
-函数原型: int toupper(int ch);
-
-函数功能: 将ch字符转换成大写字母
-
-函数返回: 与ch相应的大写字母
-
-isalnum
-
-函数名称: isalnum
-
-函数原型: int isalnum(int ch);
-
-函数功能: 检查ch是否是字母或数字
-
-函数返回: 是字母或数字返回非0,否则返回0
-
-
-isprint
-
-函数名称: isprint
-
-函数原型: int isprint(int ch);
-
-函数功能: 检查ch是否是可打印字符(包括空格),其ASCII码在0x20到0x7E之间
-
-函数返回: 是返回非0,否则返回0
-
 */
-
-
+int islower(int ch);
 /*
-
-函数名称: ispunct
-
-函数原型: int ispunct(int ch);
-
-函数功能: 检查ch是否是标点字符(不包括空格),
-即除字母,数字和空格以外的所有可打印字符
-
+isupper
+函数名称: isupper
+函数原型: int isupper(int ch);
+函数功能: 检查ch是否是大写字母(A-Z)
 函数返回: 是返回非0,否则返回0
 */
-int ispunct(int ch);
-
-
+int isupper(int ch);
+/*
+tolower
+函数名称: tolower
+函数原型: int tolower(int ch);
+函数功能: 将ch字符转换为小写字母
+函数返回: 返回ch所代表的字符的小写字母
+*/
+int tolower(int ch);
+/*
+toupper
+函数名称: toupper
+函数原型: int toupper(int ch);
+函数功能: 将ch字符转换成大写字母
+函数返回: 与ch相应的大写字母
+*/
+int toupper(int ch);
+/*
+isalnum
+函数名称: isalnum
+函数原型: int isalnum(int ch);
+函数功能: 检查ch是否是字母或数字
+函数返回: 是字母或数字返回非0,否则返回0
+*/
+int isalnum(int ch);
 /*
 isspace
-
 函数名称: isspace
-
 函数原型: int isspace(int ch);
-
 函数功能: 检查ch是否是空格符和跳格符(控制字符)或换行符
-
 函数返回: 是返回非0,否则返回0
-
 参数说明:
-
 所属文件: <ctype.h>
-
+*/
+ int isspace(int ch);
+ /*
 isxdigit
-
 函数名称: isxdigit
-
 函数原型: int isxdigit(int ch);
-
 函数功能: 检查ch是否是一个16进制数学字符(即0-9,或A-F,或a-f)
-
 函数返回: 是返回非0,否则返回0
-
 参数说明:
-
 所属文件: <ctype.h>
-
+*/
+int isxdigit(int ch);
+ /*
 isascii
-
 函数名称: isascii
-
 函数原型: int isascii(int ch)
-
 函数功能: 测试参数是否是ASCII码0-127
-
 函数返回: 是返回非0,否则返回0
-
 参数说明: ch-被测参数
-
 */
+int isascii(int ch);
 
-
-snprintf
-/*
-(1) 如果格式化后的字符串长度 < size，则将此字符串全部复制到str中，并给其后添加一个字符串结束符('\0')；
-(2) 如果格式化后的字符串长度 >= size，则只将其中的(size-1)个字符复制到str中，并给其后添加一个字符串结束符('\0')，返回值为欲写入的字符串长度。
-*/
-strncat
-//区别
