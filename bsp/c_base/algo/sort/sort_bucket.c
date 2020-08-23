c9b5193ee58af3f02d1ae257abbc2ae4b6b3bdca
andycodes <brucefan@foxmil.com>
2020-08-23 09:01:38 +0800

    sort_bucket

--- bsp/c_base/algo/sort/sort_bucket.c
+++ bsp/c_base/algo/sort/sort_bucket.c
@@ -1,5 +1,5 @@
 /**
- * 桶排序：C 语言
+ * 桶排序：
  *
  */
 
@@ -16,31 +16,26 @@
  * 参数说明：
  *     a -- 待排序数组
  *     n -- 数组a的长度
- *     max -- 数组a中最大值的范围
+ *     bucketSize -- 数组a中最大值的范围
  */
-void bucket_sort(int a[], int n, int max)
+void bucket_sort(int a[], int n, int bucketSize)
 {
 	int i, j;
-    int *buckets;
 
-    if (a==NULL || n<1 || max<1)
-        return ;
+	if (a==NULL || n<1 || bucketSize<1)
+		return ;
 
-    // 创建一个容量为max的数组buckets，并且将buckets中的所有数据都初始化为0。
-    if ((buckets=(int *)malloc(max*sizeof(int)))==NULL)
-        return ;
-    memset(buckets, 0, max*sizeof(int));
+	int buckets[bucketSize];
+	memset(buckets, 0, bucketSize*sizeof(int));
 
 	// 1. 计数
-    for(i = 0; i < n; i++)
-        buckets[a[i]]++;
+	for(i = 0; i < n; i++)
+		buckets[a[i]]++;
 
-	// 2. 排序
-	for (i = 0, j = 0; i < max; i++)
-		while( (buckets[i]--) >0 )
+	// 2. 排序:自然序
+	for (i = 0, j = 0; i < bucketSize; i++)
+		while( (buckets[i]--) >0 )//重复数字
 			a[j++] = i;
-
-	free(buckets);
 }
 
 void main()
@@ -61,3 +56,4 @@ void main()
 		printf("%d ", a[i]);
 	printf("\n");
 }
+
