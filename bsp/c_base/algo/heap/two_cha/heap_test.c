b119f5ac2c32af839a0f28121cb273714858dd8f
andycodes <brucefan@foxmil.com>
2020-11-28 21:03:30 +0800

    felix

--- bsp/c_base/algo/heap/two_cha/heap_test.c
+++ bsp/c_base/algo/heap/two_cha/heap_test.c
@@ -119,12 +119,12 @@ int** getSkyline(int** buildings, int buildingsSize, int* buildingsColSize,
 
 	int lastMaxHeight = 0;
 	for (int i = 0; i < pointCnt; i++) {
-		if (point[i][1] < 0) {
+		if (point[i][1] < 0) {//left side
 			struct heapEntry node;
 			node.x = point[i][0];
 			node.key = -point[i][1];
 			heapPush(hp, node);// 左端点，高度入堆
-		} else {
+		} else { //right side
 			/*
 			右端点不一定是最值
 			右端点，移除高度
@@ -194,3 +194,303 @@ int findMaxValueOfEquation(int** points, int pointsSize, int* pointsColSize, int
 	return res;
 }
 
+/*
+767. 重构字符串
+给定一个字符串S，检查是否能重新排布其中的字母，使得两相邻的字符不同。
+
+若可行，输出任意可行的结果。若不可行，返回空字符串。
+
+示例 1:
+
+输入: S = "aab"
+输出: "aba"
+示例 2:
+
+输入: S = "aaab"
+输出: ""
+*/
+
+struct tagNode{
+    int cnt;
+    char val;
+};
+
+int compare(const void *a, const void *b)
+{
+    return (*(struct tagNode *)a).cnt - (*(struct tagNode *)b).cnt;
+}
+
+
+
+char * reorganizeString(char * S){
+
+
+if(S == NULL)
+    return NULL;
+
+struct tagNode hash[26] = {0};
+int len = strlen(S);
+int i, j;
+char *ret = (char *)calloc(len+1, sizeof(char));
+
+
+for(i=0; i<26; i++)
+{
+    hash[i].cnt = 0;
+}
+
+for(i=0; i<len; i++)
+{
+    hash[S[i] - 'a'].cnt++;
+    hash[S[i] - 'a'].val = S[i];
+}
+
+//printf("%d %d\n", hash[0].cnt, hash[1].cnt);
+
+qsort(hash, 26, sizeof(struct tagNode), compare);
+
+//printf("%c cnt:%d %c cnt:%d %c cnt:%d\n", hash[25].val, hash[25].cnt, hash[24].val, hash[24].cnt, hash[23].val, hash[23].cnt);
+
+
+if(hash[25].cnt > (len+1)/2)
+{
+    return "";
+}
+
+//printf("len: %d", len);
+
+int ptr = 0;
+// 开始填充
+for(i=25; i>=0; i--)
+{
+    for(j=0; j<hash[i].cnt; j++, ptr+=2)
+    {
+        if(ptr >= len)
+            ptr = 1;
+        ret[ptr] = hash[i].val;
+    }
+}
+
+return ret;
+
+
+}
+
+#define MAXNUM 26
+typedef struct {
+int num;
+char key;
+}NodeMap;
+
+int CombFun(const void *a, const void *b)
+{
+NodeMap *nodeA = (NodeMap *)a;
+NodeMap *nodeB = (NodeMap *)b;
+
+return nodeB->num - nodeA->num;
+}
+
+char * reorganizeString(char * S){
+if (S == NULL) {
+return NULL;
+}
+NodeMap nodeMap[MAXNUM] = {0};
+int i;
+int len = strlen(S);
+char *outPut = NULL;
+bool result = false;
+int count = 0;
+int validNum = 0;
+
+outPut = (char *)calloc(len + 1, sizeof(char));
+for (i = 0; i < MAXNUM; i++) {
+    nodeMap[i].key = 'a' + i;
+}
+for (i = 0; i < len; i++) {
+    nodeMap[S[i] - 'a'].num++;
+}
+
+while (1) {
+    qsort(nodeMap, MAXNUM, sizeof(NodeMap), CombFun);
+    if(nodeMap[0].num == 0) {
+        break;
+    }
+     for (i = 0; i < 2; i++) {
+        if (i < MAXNUM && nodeMap[i].num != 0) {
+            nodeMap[i].num--;
+            outPut[count++] = nodeMap[i].key;
+            validNum++;
+            if (validNum == len) {
+                break;
+            }
+        } else {
+            outPut[0] = '\0';
+            return outPut;
+        }
+    }
+}
+
+return outPut;
+}
+
+typedef struct Hash {
+    char alph;
+    int num;
+} hash;
+
+int cmp(const void* a, const void* b) {
+    hash* A = (hash*)a;
+    hash* B = (hash*)b;
+    return B->num - A->num;
+}
+
+char * reorganizeString(char * S){
+    int i, j = 0, cnt;
+    if (S == "") {
+        return S;
+    }
+
+    hash hashTable[26] = {0};
+    int len = strlen(S);
+    for (int i = 0; i < len; i++) {
+        hashTable[S[i] - 'a'].alph = S[i];
+        hashTable[S[i] - 'a'].num++;
+    }
+    qsort(hashTable, 26,  sizeof(hash), cmp);
+    if (hashTable[0].num > (len + 1) / 2) {
+        return "";
+    }
+
+    for (i = 0; i < 26; i++) {
+        cnt = hashTable[i].num;
+        if (cnt == 0) {
+            break;
+        }
+        char alph = hashTable[i].alph;
+        while (cnt--) {
+            S[j] = alph;
+            if (j + 2 >= len) {  //此分支只会出现一次，画一下就清楚了
+                j = 1;
+            } else {
+                j += 2;
+            }
+        }
+    }
+
+    return S;
+}
+
+/*
+1642. 可以到达的最远建筑
+给你一个整数数组 heights ，表示建筑物的高度。另有一些砖块 bricks 和梯子 ladders 。
+
+你从建筑物 0 开始旅程，不断向后面的建筑物移动，期间可能会用到砖块或梯子。
+
+当从建筑物 i 移动到建筑物 i+1（下标 从 0 开始 ）时：
+
+如果当前建筑物的高度 大于或等于 下一建筑物的高度，则不需要梯子或砖块
+如果当前建筑的高度 小于 下一个建筑的高度，您可以使用 一架梯子 或 (h[i+1] - h[i]) 个砖块
+如果以最佳方式使用给定的梯子和砖块，返回你可以到达的最远建筑物的下标（下标 从 0 开始 ）
+*/
+int furthestBuilding(int* heights, int heightsSize, int bricks, int ladders)
+{
+	struct HeapCtrl *hp = heapInit(ladders + 1);
+
+	int sumh = 0;
+	for (int i = 1; i < heightsSize; i++) {
+		int diffhigh = heights[i] - heights[i - 1];
+		if (diffhigh <= 0) {
+			continue;
+		}
+
+		struct heapEntry node;
+		node.key = diffhigh;
+		heapPush(hp, node);
+
+		if (heapSize(hp) > ladders) {
+			sumh += heapTop(hp);
+			heapPop(hp);
+		}
+
+		if (sumh > bricks) {
+			return i - 1;
+		}
+	}
+
+	return heightsSize - 1;
+}
+
+/*
+1054. 距离相等的条形码
+在一个仓库里，有一排条形码，其中第 i 个条形码为 barcodes[i]。
+
+请你重新排列这些条形码，使其中两个相邻的条形码 不能 相等。 你可以返回任何满足该要求的答案，此题保证存在答案。
+
+
+
+示例 1：
+
+输入：[1,1,1,2,2,2]
+输出：[2,1,2,1,2,1]
+*/
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+#define MAX_CNT 10001
+typedef struct {
+    int cnt;  /* 数字个数 */
+    int num;  /* 数字的值 */
+}DATA;
+DATA g_array[MAX_CNT];
+int cmp(void* a, void* b)
+{
+    DATA* aa = (DATA*)a;
+    DATA* bb = (DATA*)b;
+    if (aa->cnt == bb->cnt) {
+        return bb->num - aa->num;
+    }
+    return bb->cnt - aa->cnt;
+}
+int* rearrangeBarcodes(int* barcodes, int barcodesSize, int* returnSize){
+    *returnSize = barcodesSize;
+    if (barcodesSize < 3) {
+        return barcodes;
+    }
+    memset(g_array, 0, sizeof(DATA));
+    for (int i = 0; i < barcodesSize; i++) {
+        g_array[barcodes[i]].cnt++;
+        g_array[barcodes[i]].num = barcodes[i];
+    }
+    qsort(g_array, MAX_CNT, sizeof(DATA), cmp);
+    memset(barcodes, 0, sizeof(int));
+    /* 已经排序完成，先放奇数位子，然后再放偶数位子 */
+    int i = 0;
+    int j = 0;
+    while (i < barcodesSize) {
+        if (j > MAX_CNT) {
+            break;
+        }
+        if (g_array[j].cnt > 0) {
+            barcodes[i] = g_array[j].num;
+            g_array[j].cnt--;
+            i += 2;
+        } else {
+            j++;
+        }
+    }
+    i = 1;
+    /* 奇数放完后排序，然后把剩余的放在偶数位 */
+    while (i < barcodesSize) {
+        if (j > MAX_CNT) {
+            break;
+        }
+        if (g_array[j].cnt > 0) {
+            barcodes[i] = g_array[j].num;
+            g_array[j].cnt--;
+            i += 2;
+        } else {
+            j++;
+        }
+    }
+    return barcodes;
+}
