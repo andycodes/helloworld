aef59d9447fe4f4ee5fd00c1e05f0a4361df0913
andycodes <brucefan@foxmail.com>
2021-07-17 07:47:41 +0800

    felix

--- bsp/c_base/elf/elf_analysis.c
+++ bsp/c_base/elf/elf_analysis.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-int global_init_var = 84;
-int global_uninit_var;
+int global_init_var = 84; //.data
+int global_uninit_var; //*COM*
+static int sg_init_var = 77; //.data
+static int sg_uninit_var; //.bss
 
 void func1(int i)
 {
@@ -10,13 +12,19 @@ void func1(int i)
 
 int main(void)
 {
-	static int static_init_var = 85;
-	static int static_uninit_val;
+	static int static_init_var = 85; //.data
+	static int static_uninit_val; //.bss
 	int a = 1;
 	int b;
 
-	func1(static_init_var + static_uninit_val + a + b);
+	func1(static_init_var + static_uninit_val + a + b); //.text
+	func1(sg_init_var + sg_uninit_var);
 
 	return 0;
 }
+
+/*
+arm-linux-gnueabi-gcc -c elf_analysis.c
+arm-linux-gnueabi-objdump -x elf_analysis.o
+*/
 
