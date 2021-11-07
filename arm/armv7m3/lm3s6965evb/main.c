1f75a7fbed2e760023fbcba16467f2f9d13ceca4
andycodes <brucefan@foxmail.com>
2021-11-07 14:11:56 +0800

    felix

--- arm/armv7m3/lm3s6965evb/main.c
+++ arm/armv7m3/lm3s6965evb/main.c
@@ -21,8 +21,13 @@ void task1_entry(void *param)
     init_systick(10);
     for(;;) {
         printk("%s\n", __func__);
-        task_delay_s(1);
+        task_delay_s(2);
     }
+}
+
+void delay(uint32_t delay)
+{
+    while(delay--);
 }
 
 void task2_entry(void *param)
@@ -30,28 +35,42 @@ void task2_entry(void *param)
 
     for(;;) {
         printk("%s\n", __func__);
-        task_delay_s(2);
+        delay(65536000);
+    }
+}
+
+void task3_entry(void *param)
+{
+    for(;;) {
+        printk("%s\n", __func__);
+        delay(65536000);
     }
 }
 
 task_t task1;
 task_t task2;
+task_t task3;
 task_stack_t task1_stk[1024];
 task_stack_t task2_stk[1024];
+task_stack_t task3_stk[1024];
 
 int main()
 {
 
     clear_bss();
 
-    DEBUG("Hello FELIX RTOS C0.7\n");
+    DEBUG("Hello FELIX RTOS C0.8\n");
     DEBUG("psp:0x%x\n", get_psp());
     DEBUG("msp:0x%x\n", get_msp());
 
+    init_task_module();
+
     task_init(&task1, task1_entry, (void *)0x11111111, 0, &task1_stk[1024]);
     task_init(&task2, task2_entry, (void *)0x22222222, 1, &task2_stk[1024]);
+    task_init(&task3, task3_entry, (void *)0x33333333, 1, &task3_stk[1024]);
 
-    init_task_module();
+    g_next_task = task_highest_ready();
+    task_run_first();
 
     for(;;);
     return 0;
