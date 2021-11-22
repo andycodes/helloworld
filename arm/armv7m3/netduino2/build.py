687b65a714a8526c0fc20201b2bcf616eada8f62
andycodes <brucefan@foxmail.com>
2021-11-22 22:39:26 +0800

    felix

---
+++ arm/armv7m3/netduino2/build.py
@@ -0,0 +1,27 @@
+#!/user/bin/python
+#!conding=utf8
+
+import glob 
+import os
+import shutil
+
+
+if os.path.exists("./obj"):
+	shutil.rmtree("./obj")
+	shutil.rmtree("./bin")
+	
+os.mkdir("obj")
+os.mkdir("bin")	
+
+os.system("make")
+
+g = os.walk("./src")
+for path,d,filelist in g:
+	#print d;
+	for filename in filelist:
+		#print os.path.join(path, filename)
+                if filename.endswith(".o"):
+                    #print filename
+                    file_path = os.path.join(path, filename)
+                    print file_path
+                    shutil.move(file_path, "./obj")
