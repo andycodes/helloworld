7d3e1a49571547ae47788d72362a47111a716393
andycodes <brucefan@foxmail.com>
2021-11-23 23:02:01 +0800

    felix

--- arm/armv7m3/netduino2/build.sh
+++
@@ -1,8 +0,0 @@
-#if [ -n "$1" ]; then
-#    rm startup_m3.o main.o startup.elf startup.bin
-#else
-    arm-none-eabi-as -mcpu=cortex-m4 -mthumb startup_m3.s -g -o startup_m3.o
-    arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -c main.c -o main.o
-    arm-none-eabi-ld -T m3.ld  startup_m3.o main.o -o startup.elf
-    arm-none-eabi-objcopy -O binary startup.elf startup.bin
-#fi
