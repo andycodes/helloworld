3f28e4d6f5394ea13449f6df2054137f99187ed1
andycodes <brucefan@foxmail.com>
2021-12-05 17:24:29 +0800

    felix

--- arm/cortexm/build.py
+++ arm/cortexm/build.py
@@ -6,7 +6,13 @@ import os
 import shutil
 import sys
 
-#    python build.py mps2-an505
+#   python build.py mps3-an547
+# 	make -f makefile_armv8m board=mps3-an547 qemu
+
+#   python build.py mps2-an505
+# 	make -f makefile_armv8m board=mps2-an505 qemu
+# 	make -f makefile_armv8m board=mps2-an505 gdbserver
+# 	make -f makefile_armv8m board=mps2-an505 gdb
 
 ARCH = 'armv7m'
 BOARD_LISTS = ['mps3-an547', 'mps2-an505']
