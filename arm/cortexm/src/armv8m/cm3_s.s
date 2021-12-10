ab38850cf00f0c0fc473674b270cc489c93cf613
andycodes <brucefan@foxmail.com>
2021-12-10 19:56:24 +0800

    felix

--- arm/cortexm/src/armv8m/cm3_s.s
+++ arm/cortexm/src/armv8m/cm3_s.s
@@ -35,6 +35,36 @@ get_control_reg:
     mrs     r0, CONTROL
     blx     lr
 
+pendsv_handler:
+    /*CM3 will push the r0-r3, r12, r14, r15, xpsr by hardware*/
+    mrs     r0, psp
+    cbz     r0, pendsv_handler_nosave
+
+    /* g_current_task->psp-- = r11;
+     * ...
+     * g_current_task->psp-- = r4;
+     * g_current_task->stack = psp;
+     */
+    stmdb   r0!, {r4-r11}
+    ldr     r1, =g_current_task
+    ldr     r1, [r1]
+    str     r0, [r1]
+
+pendsv_handler_nosave:
+
+    /* *g_current_task = *g_next_task */
+    ldr     r0, =g_current_task
+    ldr     r1, =g_next_task
+    ldr     r2, [r1]
+    str     r2, [r0]
+
+    /*r0 = g_current_task->stack*/
+    ldr     r0, [r2]
+    ldmia   r0!, {r4-r11}
+
+    msr     psp, r0
+    orr     lr, lr, #0x04   /*Swtich to PSP*/
+    bx      lr
 
 get_primask:
     mrs     r0, PRIMASK
--- arm/cortexm/src/armv8m/main.c
+++ arm/cortexm/src/armv8m/main.c
@@ -5,10 +5,26 @@
 #include "board.h"
 #include "cmsis_gcc.h"
 
+extern int testcase_task();
+
+extern uint32_t _bss;
+extern uint32_t _ebss;
+extern void testcase(void);
+
+static inline void clear_bss(void)
+{
+    uint8_t *start = (uint8_t *)_bss;
+    while ((uint32_t)start < _ebss) {
+        *start = 0;
+        start++;
+    }
+}
 
 void __PROGRAM_START(void)
 {
+    clear_bss();
     board_init();
     printk("hello %s ^-^^-^^-^^-^\n", board_info());
+    testcase_task();
     testcase();
 }
