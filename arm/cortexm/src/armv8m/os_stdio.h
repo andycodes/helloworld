cb2d1a0fcec77da992c998bb134cc522e9ff441c
andycodes <brucefan@foxmail.com>
2021-12-19 22:08:54 +0800

    felix

--- arm/cortexm/src/armv8m/os_stdio.h
+++ arm/cortexm/src/armv8m/os_stdio.h
@@ -3,6 +3,55 @@
 
 #include <stdint.h>
 
+#define  EOK                          0               /**< There is no error */
+#define  ERROR                        1               /**< A generic error happens */
+#define  ETIMEOUT                     2               /**< Timed out */
+#define  EFULL                        3               /**< The resource is full */
+#define  EEMPTY                       4               /**< The resource is empty */
+#define  ENOMEM                       5               /**< No memory */
+#define  ENOSYS                       6               /**< No system */
+#define  EBUSY                        7               /**< Busy */
+#define  EIO                          8               /**< IO error */
+#define  EINTR                        9               /**< Interrupted system call */
+#define  EINVAL                       10              /**< Invalid argument */
+
+    #define SECTION(x)                  __attribute__((section(x)))
+    #define RT_UNUSED                   __attribute__((unused))
+    #define RT_USED                     __attribute__((used))
+    #define ALIGN(n)                    __attribute__((aligned(n)))
+    #define RT_WEAK                     __attribute__((weak))
+
+
+#define RT_ASSERT(EX)  
+
+/**@}*/
+
+/**
+ * @ingroup BasicDef
+ *
+ * @def RT_ALIGN(size, align)
+ * Return the most contiguous size aligned at specified width. RT_ALIGN(13, 4)
+ * would return 16.
+ */
+#define RT_ALIGN(size, align)           (((size) + (align) - 1) & ~((align) - 1))
+
+/**
+ * @ingroup BasicDef
+ *
+ * @def RT_ALIGN_DOWN(size, align)
+ * Return the down number of aligned at specified width. RT_ALIGN_DOWN(13, 4)
+ * would return 12.
+ */
+#define RT_ALIGN_DOWN(size, align)      ((size) & ~((align) - 1))
+
+/**
+ * @ingroup BasicDef
+ *
+ * @def RT_NULL
+ * Similar as the \c NULL in C library.
+ */
+#define RT_NULL                         (0)
+
 extern void printk(const char *fmt, ...);
 extern int memset(void *mem, uint8_t val, uint32_t sz);
 extern int memcpy(void *dst, const void *src, uint32_t sz);
