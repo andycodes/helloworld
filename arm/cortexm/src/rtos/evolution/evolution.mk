f78c029d57a1bd17340a08d8494c56c556dc6aa5
andycodes <brucefan@foxmail.com>
2021-12-19 15:51:47 +0800

    felix

---
+++ arm/cortexm/src/rtos/evolution/evolution.mk
@@ -0,0 +1,13 @@
+
+
+OBJS += src/rtos/evolution/kernel/task.o \
+		src/rtos/evolution/kernel/lib.o \
+		src/rtos/evolution/kernel/event.o \
+		src/rtos/evolution/kernel/sem.o \
+		src/rtos/evolution/kernel/mailbox.o \
+		src/rtos/evolution/kernel/memblock.o \
+		src/rtos/evolution/kernel/timer.o \
+		src/rtos/evolution/kernel/mutex.o \
+		src/rtos/evolution/kernel/flag_group.o
+		
+		
