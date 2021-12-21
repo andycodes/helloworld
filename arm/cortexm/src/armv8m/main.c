1495020851451d7fc3ff3995883f7feed061db78
andycodes <brucefan@foxmail.com>
2021-12-21 22:31:21 +0800

    felix

--- arm/cortexm/src/armv8m/main.c
+++ arm/cortexm/src/armv8m/main.c
@@ -24,10 +24,20 @@ int rt_secure_svc_handle(int svc_id, rt_ubase_t arg0, rt_ubase_t arg1, rt_ubase_
 void rt_trustzone_context_load(rt_ubase_t context)
 {}
 
+void SVC1_Handler(void)
+{
+    printk("hello %s ^-^^-^^-^^-^\n", __func__);
+}
+
 void __PROGRAM_START(void)
 {
     clear_bss();
     board_init();
     printk("hello %s ^-^^-^^-^^-^\n", board_info());
+
+ //int tzcall(int id, rt_ubase_t arg0, rt_ubase_t arg1, rt_ubase_t arg2);
+
+    svcall();
+
     module_init();
 }
