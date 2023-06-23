2c98912df6a66a4e59c01baab4fcb26e410cdfd4
andycodes <brucefan@foxmail.com>
2023-06-23 22:32:23 +0800

    clear project

--- arm/cortexm/src/platform/lm3s6965evb/board.mk
+++ arm/cortexm/src/platform/lm3s6965evb/board.mk
@@ -3,4 +3,4 @@ CFLAGS += -Isrc/cmsis/Device/ARM/ARMCM3/Include
 
 CSRC += src/cmsis/Device/ARM/ARMCM3/Source/system_ARMCM3.c 
 CSRC += src/cmsis/Device/ARM/ARMCM3/Source/startup_ARMCM3.c
-CSRC += src/platform/$(board)/dfx.c
+CSRC += src/$(ARCH)/dfx.c
