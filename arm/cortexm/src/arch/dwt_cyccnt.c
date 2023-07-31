c503fee25e6b17e267a0ae440be0f0b25dc44c08
andycodes <brucefan@foxmail.com>
2023-08-01 07:59:28 +0800

    felix

--- arm/cortexm/src/arch/dwt_cyccnt.c
+++ arm/cortexm/src/arch/dwt_cyccnt.c
@@ -77,10 +77,6 @@ void stop_timer(){
     *DWT_CONTROL = *DWT_CONTROL | 0 ; // disable the counter    
 }
  
-unsigned int getCycles(){
-    return *DWT_CYCCNT;
-}
-
 ////////////////////////
  
 /**
@@ -127,26 +123,6 @@ unsigned int getCycles(){
 
 
 /**
-  * @brief  初始化时间戳
-  * @param  无
-  * @retval 无
-  * @note   使用延时函数前，必须调用本函数
-  */
-HAL_StatusTypeDef HAL_InitTick(uint32_t TickPriority)
-{
-    /* 使能DWT外设 */
-    DEM_CR |= (uint32_t)DEM_CR_TRCENA;                
-
-    /* DWT CYCCNT寄存器计数清0 */
-    DWT_CYCCNT = (uint32_t)0u;
-
-    /* 使能Cortex-M DWT CYCCNT寄存器 */
-    DWT_CR |= (uint32_t)DWT_CR_CYCCNTENA;
-  
-    return HAL_OK;
-}
-
-/**
   * @brief  读取当前时间戳
   * @param  无
   * @retval 当前时间戳，即DWT_CYCCNT寄存器的值
@@ -164,54 +140,6 @@ uint32_t CPU_TS_TmrRd(void)
 uint32_t HAL_GetTick(void)
 {        
   return ((uint32_t)DWT_CYCCNT/SysClockFreq*1000);
-}
-
-
-/**
-  * @brief  采用CPU的内部计数实现精确延时，32位计数器
-  * @param  us : 延迟长度，单位1 us
-  * @retval 无
-  * @note   使用本函数前必须先调用CPU_TS_TmrInit函数使能计数器，
-            或使能宏CPU_TS_INIT_IN_DELAY_FUNCTION
-            最大延时值为8秒，即8*1000*1000
-  */
-void CPU_TS_Tmr_Delay_US(uint32_t us)
-{
-  uint32_t ticks;
-  uint32_t told,tnow,tcnt=0;
-
-  /* 在函数内部初始化时间戳寄存器， */  
-#if (CPU_TS_INIT_IN_DELAY_FUNCTION)  
-  /* 初始化时间戳并清零 */
-  HAL_InitTick(5);
-#endif
-  
-  ticks = us * (GET_CPU_ClkFreq() / 1000000);  /* 需要的节拍数 */      
-  tcnt = 0;
-  told = (uint32_t)CPU_TS_TmrRd();         /* 刚进入时的计数器值 */
-
-  while(1)
-  {
-    tnow = (uint32_t)CPU_TS_TmrRd();  
-    if(tnow != told)
-    { 
-        /* 32位计数器是递增计数器 */    
-      if(tnow > told)
-      {
-        tcnt += tnow - told;  
-      }
-      /* 重新装载 */
-      else 
-      {
-        tcnt += UINT32_MAX - told + tnow; 
-      } 
-      
-      told = tnow;
-
-      /*时间超过/等于要延迟的时间,则退出 */
-      if(tcnt >= ticks)break;
-    }  
-  }
 }
 
 /*********************************************END OF FILE**********************/
