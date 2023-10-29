e209269a08e27f6ebb56bd712a69dda50e2bfaa2
andycodes <brucefan@foxmail.com>
2023-10-29 10:22:22 +0800

    riscv gdb

--- arm/cortexm/qemu_riscv_t.sh
+++ arm/cortexm/qemu_riscv_t.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
-make qemu board=mps3-an547
+#make qemu board=mps3-an547
 #make gdbserver board=mps3-an547
 #make gdb board=mps3-an547
+#（gdb）target remote :8886
+
+gdb-multiarch bin/riscv/felix.elf 
 #（gdb）target remote :8886
