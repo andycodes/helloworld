e82fba88fae32f0d11de3657fbb238a0c5ddcd0f
andycodes <brucefan@foxmail.com>
2024-03-17 09:48:30 +0800

    felix

--- clang_cmake_so/debug.sh
+++ clang_cmake_so/debug.sh
@@ -1,1 +1,10 @@
-/project/gcc/arm-gnu-toolchain-13.2.Rel/bin/arm-none-eabi-objdump -x -s -S /project/helloworld/clang_cmake_so/bin/frank >bin/frank.dump
+
+OUT_PATH=/project/helloworld/clang_cmake_so/bin/
+OBJ=${OUT_PATH}frank
+COMPILER_PATH=/project/compiler/llvm_clang/clang/bin/
+DUMPTOOL=${COMPILER_PATH}llvm-objdump
+READELF=${COMPILER_PATH}llvm-readelf
+
+file ${OBJ}
+${DUMPTOOL} -D  --disassemble-zeroes ${OBJ} >${OUT_PATH}frank.dump
+${READELF} --all ${OBJ} >${OUT_PATH}frank.readelf
