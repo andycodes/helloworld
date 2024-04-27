734896f28797cfd873b0c7a4f007550d17765283
andycodes <brucefan@foxmail.com>
2024-04-27 10:01:42 +0800

    felix

--- linux_kernel/kernel/codeContext/kthread/kernel_thread.c
+++ linux_kernel/kernel/codeContext/kthread/kernel_thread.c
@@ -16,16 +16,14 @@ struct task_struct *thread_t = NULL;
 /*建议使用 wait_event_interruptible  简单*/
 static int test_thread(void *data)
 {
-	struct sched_param param = {.sched_priority = 1};
 	DECLARE_WAITQUEUE(wait, current);
 
-	sched_setscheduler(current, SCHED_RR, &param);
 	add_wait_queue(&apm_waitqueue, &wait);
 
 	set_current_state(TASK_INTERRUPTIBLE);
 	while(!kthread_should_stop()) {
 		schedule();
-		pr_info("test_thread.\n");
+		pr_info("%s.\n", __func__);
 		__set_current_state(TASK_INTERRUPTIBLE);
 	}
 
@@ -34,11 +32,11 @@ static int test_thread(void *data)
 	return 0;
 }
 
-static int thread_test(void *data)
+static int test_run(void *data)
 {
-	msleep(5000);
+	pr_info("%s.\n", __func__);
+	msleep(500);
 	wake_up_interruptible(&apm_waitqueue);
-	pr_info("thread_t.\n");
 	return 0;
 }
 
@@ -55,17 +53,11 @@ static int thread_test(void *data)
 
 	wake_up_process(task_test);//启动线程
 
-	thread_t = kthread_run(thread_test, NULL, "thread_test");
+
+
+	thread_t = kthread_run(test_run, NULL, "test_run");
 	return 0;
 }
 
- void kthread_exit(void)
-{
-	//if (task_test) kthread_stop(task_test);
-	//if (thread_t) kthread_stop(thread_t);
-}
-
 module_init(kernel_thread_init);
-module_exit(kthread_exit);
 MODULE_LICENSE("Dual BSD/GPL");
-
