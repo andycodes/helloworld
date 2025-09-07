20af2b4d39221ee2adeb9cd9b603d3b86a68e25d
andycodes <brucefan@foxmail.com>
2025-09-07 23:04:04 +0800

    finance

--- finance/fan_akshare.py
+++ finance/fan_akshare.py
@@ -1,10 +1,16 @@
 import akshare as ak
+from datetime import datetime, date, timedelta
 
-# 获取腾讯控股（0700.HK）的历史行情数据
-stock_hk_hist = ak.stock_hk_hist(symbol="00700", period="daily", start_date="20250901", end_date="20250907")
-print(stock_hk_hist.head())
+stocks = ["00700"]
 
-# 获取港股实时行情（示例）
-# 注意：AkShare的实时数据接口可能因数据源变动而调整，请查阅其最新文档
-# stock_hk_spot = ak.stock_hk_spot() 
-# print(stock_hk_spot.head())
+def get_n_days_ago(n):
+    n_days_ago = datetime.now() - timedelta(days=n)
+    return n_days_ago.strftime("%Y%m%d")
+
+def get_date_today():
+    today = date.today()
+    return today.strftime("%Y%m%d")
+
+for stock in stocks:
+    stock_hk_hist = ak.stock_hk_hist(symbol=stock, period="daily", start_date=get_n_days_ago(7), end_date=get_date_today())
+    print(stock_hk_hist.head())
