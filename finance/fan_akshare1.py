f140a9ea2d898be59bc586a30a563f7bbb9bc228
andycodes <brucefan@foxmail.com>
2025-10-25 09:53:57 +0800

    felix

---
+++ finance/fan_akshare1.py
@@ -0,0 +1,11 @@
+import akshare as ak
+
+stock_yjbb_em_df = ak.stock_yjbb_em(date="20250930")
+print(stock_yjbb_em_df)
+roe_df = stock_yjbb_em_df[['股票代码', '股票简称', '净资产收益率']]
+
+# 按 ROE 倒序排列
+roe_df_sorted = roe_df.sort_values(by='净资产收益率', ascending=False)
+
+# 打印结果
+print(roe_df_sorted)
---
+++ finance/fan_akshare_get_stock_code_id.py
@@ -0,0 +1,4 @@
+import akshare as ak
+print(ak.__version__)
+stock_info_a_code_name_df = ak.stock_info_a_code_name()
+print(stock_info_a_code_name_df)
